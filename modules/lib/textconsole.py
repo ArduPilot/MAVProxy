7679a264a364426b4befcdeb4a148d205f19279a
Andrew Tridgell <tridge@samba.org>
2012-05-17 13:06:25 +1000

    ensure objects are converted to text

--- modules/lib/textconsole.py
+++ modules/lib/textconsole.py
@@ -14,11 +14,16 @@ class SimpleConsole():
 
     def write(self, text, fg='black', bg='white'):
         '''write to the console'''
-        sys.stdout.write(text)
+        if isinstance(text, str):
+            sys.stdout.write(text)
+        else:
+            sys.stdout.write(str(text))
         sys.stdout.flush()
 
     def writeln(self, text, fg='black', bg='white'):
         '''write to the console with linefeed'''
+        if not isinstance(text, str):
+            text = str(text)
         self.write(text + '\n', fg=fg, bg=bg)
 
     def set_status(self, name, text='', row=0, fg='black', bg='white'):
