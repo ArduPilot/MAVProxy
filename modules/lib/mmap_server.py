2a9b47935f41d95f98a541e22a75a69cdedc4c7b
wiseman <jjwiseman@gmail.com>
2012-06-23 21:49:00 -0700

    Got an HTTP server running.

--- modules/lib/mmap_server.py
+++ modules/lib/mmap_server.py
@@ -1,60 +1,32 @@
 import BaseHTTPServer
-import cgi
 import json
-import signal
+import logging
 import thread
-import time
-import urlparse
 
 
-class Server():
-  def __init__(self, handler, port=9999, mp_state=None):
-    self.server = BaseHTTPServer.HTTPServer(('', port), handler)
-    self.port = port
+class Server(BaseHTTPServer.HTTPServer):
+  def __init__(self, handler, address='', port=9999, module_state=None):
+    BaseHTTPServer.HTTPServer.__init__(self, (address, port), handler)
     self.allow_reuse_address = True
-    self.done = False
-
-  def serve(self):
-    signal.signal(signal.SIGINT, self.terminate)
-    thread.start_new_thread(self.server.serve_forever, ())
-    while not self.done:
-      try:
-        time.sleep(0.3)
-      except IOError:
-        pass
-    self.server.server_close()
-
-  def start(self):
-    thread.start_new_thread(self.serve, self)
-
-  def terminate(self, unused_sig_num, unused_frame):
-    self.done = True
-
-  def loop(self):
-    url = "http://127.0.0.1:%s" % self.port
-    print "server running on port %s" % self.port
-    print "documentation and usage is available at %s/\n\n" % url
-
-    self.serve()
+    self.module_state = module_state
 
 
 class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
-  def __init__(self, request, client_address, server):
-    self.ctx = {}
-    self.points = {}
-    BaseHTTPServer.BaseHTTPRequestHandler.__init__(
-      self, request, client_address, server)
-
   def do_GET(self):
     #scheme, host, path, params, query, frag = urlparse.urlparse(self.path)
     #params = cgi.parse_qs(query)
-    pass
+    print('HOOOAH')
+    state = self.server.module_state
+    data = {'lat': state.lat,
+            'lon': state.lon,
+            'heading': state.heading}
+    self.send_response(200)
+    self.end_headers()
+    self.wfile.write(json.dumps(data))
 
 
-def main():
-  server = Server(Handler, 9999)
-  server.loop()
-
-
-if __name__ == '__main__':
-  main()
+def start_server(address, port, module_state):
+  server = Server(
+    Handler, address=address, port=port, module_state=module_state)
+  thread.start_new_thread(server.serve_forever, ())
+  return server
