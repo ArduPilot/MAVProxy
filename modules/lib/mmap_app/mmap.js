44575e0ada271a58b12a229c909ab6a35237eedc
wiseman <jjwiseman@gmail.com>
2012-06-28 15:10:51 -0700

    Delinting

--- modules/lib/mmap_app/mmap.js
+++ modules/lib/mmap_app/mmap.js
@@ -4,47 +4,47 @@ function MapPanner(map) {
     this.targetLocation = null;
     
     this.animateToCenter = function() {
-	var need_more_animation = false;
-	var curLocation = theMapPanner.map.getCenter();
-	var nextLat = curLocation.lat + (theMapPanner.targetLocation.lat - curLocation.lat) * 0.2;
-	var nextLon = curLocation.lon + (theMapPanner.targetLocation.lon - curLocation.lon) * 0.2;
-	if (Math.abs(nextLat - curLocation.lat) < 0.00001) {
-	    nextLat = theMapPanner.targetLocation.lat;
-	} else {
-	    need_more_animation = true;
-	}
-	if (Math.abs(nextLon - curLocation.lon) < 0.00001) {
-	    nextLon = theMapPanner.targetLocation.lon;
-	} else {
-	    need_more_animation = true;
-	}
+        var need_more_animation = false;
+        var curLocation = theMapPanner.map.getCenter();
+        var nextLat = curLocation.lat + (theMapPanner.targetLocation.lat - curLocation.lat) * 0.2;
+        var nextLon = curLocation.lon + (theMapPanner.targetLocation.lon - curLocation.lon) * 0.2;
+        if (Math.abs(nextLat - curLocation.lat) < 0.00001) {
+            nextLat = theMapPanner.targetLocation.lat;
+        } else {
+            need_more_animation = true;
+        }
+        if (Math.abs(nextLon - curLocation.lon) < 0.00001) {
+            nextLon = theMapPanner.targetLocation.lon;
+        } else {
+            need_more_animation = true;
+        }
 
-	theMapPanner.map.setCenter(new MM.Location(nextLat, nextLon));
-	if (need_more_animation) {
-	    MM.getFrame(this.animateToCenter);
-	}
-    }
+        theMapPanner.map.setCenter(new MM.Location(nextLat, nextLon));
+        if (need_more_animation) {
+            MM.getFrame(this.animateToCenter);
+        }
+    };
 
     this.setCenter = function(location) {
-	this.targetLocation = location;
-	MM.getFrame(this.animateToCenter);
-    }
+        this.targetLocation = location;
+        MM.getFrame(this.animateToCenter);
+    };
 
 }
 
 function TrailPlotter(marker_clip) {
-    this.marker_clip = marker_clip
+    this.marker_clip = marker_clip;
 
     var last_trail_time = 0;
 
     this.update = function() {
-	var now = new Date().getTime();
-	if (last_trail_time == 0 || now - last_trail_time > 10000) {
+        var now = new Date().getTime();
+        if (last_trail_time === 0 || now - last_trail_time > 10000) {
             var trail_marker = this.marker_clip.createDefaultMarker(2);
             this.marker_clip.addMarker(trail_marker, new MM.Location(state.lat, state.lon));
             last_trail_time = now;
-	}
-    }
+        }
+    };
 }
 
 
@@ -60,84 +60,84 @@ function ADI(container) {
     console.log('CONTAINER ' + containerElement.offsetWidth + 'x' + containerElement.offsetHeight);
     this.stage = new Kinetic.Stage({
         container: container,
-	height: containerElement.offsetHeight,
-	width: containerElement.offsetWidth
+        height: containerElement.offsetHeight,
+        width: containerElement.offsetWidth
     });
     this.stage.setScale(containerElement.offsetWidth / 100.0,
-			containerElement.offsetHeight / 100.0)
+                        containerElement.offsetHeight / 100.0);
     this.layer = new Kinetic.Layer();
     this.plane = new Kinetic.Path({
         x: 50,
         y: 50,
         data: "m -26,0 15,0 3,3 M -1,0 l 2,0 M 26,0 l -15,0 -3,3",
-	stroke: "black",
-	lineJoin: "round",
-	strokeWidth: 2.5,
-	shadow: {
-	    color: 'black',
-	    blur: 5,
-	    offset: [1, 1],
-	    alpha: 0.5
-	},
+        stroke: "black",
+        lineJoin: "round",
+        strokeWidth: 2.5,
+        shadow: {
+            color: 'black',
+            blur: 5,
+            offset: [1, 1],
+            alpha: 0.5
+        },
         scale: 1.2
     });
     // add the shape to the layer
     this.layer.add(this.plane);
     this.horizon = new Kinetic.Path({
-	x: 50,
-	y: 50,
-	data: "m -100,0 200,0",
-	stroke: "black",
-	lineJoin: "round",
-	strokeWidth: 1.7,
-	scale: 1
+        x: 50,
+        y: 50,
+        data: "m -100,0 200,0",
+        stroke: "black",
+        lineJoin: "round",
+        strokeWidth: 1.7,
+        scale: 1
     });
     this.layer.add(this.horizon);
     // add the layer to the stage
     this.stage.add(this.layer);
 
     this.drawFromAttitude = function(pitch, roll) {
-	var horizon_y = 50.0 + 50.0 * Math.sin(pitch);
-	theADI.horizon.setY(horizon_y);
-	theADI.horizon.setRotation(-roll);
-	theADI.layer.draw();
-    }
+        var horizon_y = 50.0 + 50.0 * Math.sin(pitch);
+        theADI.horizon.setY(horizon_y);
+        theADI.horizon.setRotation(-roll);
+        theADI.layer.draw();
+    };
 
     this.drawFromAttitude(this.pitch, this.roll);
     
     this.animateToAttitude = function() {
-	var need_more_animation = false;
-	
-	var curPitch = theADI.pitch;
-	var nextPitch = curPitch + (theADI.targetPitch - curPitch) * 0.05;
-	if (Math.abs(nextPitch - curPitch) < 0.001) {
-	    nextPitch = theADI.targetPitch;
-	} else {
-	    need_more_animation = true;
-	}
-	
-	var curRoll = theADI.roll;
-	var nextRoll = curRoll + (theADI.targetRoll - curRoll) * 0.05;
-	if (Math.abs(nextRoll - curRoll) < 0.001) {
-	    nextRoll = theADI.targetRoll;
-	} else {
-	    need_more_animation = true;
-	}
-	
-	theADI.drawFromAttitude(nextPitch, nextRoll);
-	
-	theADI.pitch = nextPitch;
-	theADI.roll = nextRoll;
-	if (need_more_animation) {
-	    MM.getFrame(theADI.animateToAttitude);
-	}
-    }
+        var need_more_animation = false;
+        
+        var curPitch = theADI.pitch;
+        var nextPitch = curPitch + (theADI.targetPitch - curPitch) * 0.05;
+        if (Math.abs(nextPitch - curPitch) < 0.001) {
+            nextPitch = theADI.targetPitch;
+        } else {
+            need_more_animation = true;
+        }
+        
+        var curRoll = theADI.roll;
+        var nextRoll = curRoll + (theADI.targetRoll - curRoll) * 0.05;
+        if (Math.abs(nextRoll - curRoll) < 0.001) {
+            nextRoll = theADI.targetRoll;
+        } else {
+            need_more_animation = true;
+        }
+        
+        theADI.drawFromAttitude(nextPitch, nextRoll);
+        
+        theADI.pitch = nextPitch;
+        theADI.roll = nextRoll;
+        if (need_more_animation) {
+            MM.getFrame(theADI.animateToAttitude);
+        }
+    };
     
     this.setAttitude = function(pitch, roll) {
-	this.targetPitch = pitch;
-	this.targetRoll = roll;
-	MM.getFrame(this.animateToAttitude);
-    }
+        this.targetPitch = pitch;
+        this.targetRoll = roll;
+        MM.getFrame(this.animateToAttitude);
+    };
 }
 
 
@@ -193,8 +193,8 @@ function MarkerClip(map) {
     };
 
     this.setMarkerLocation = function(index, location) {
-	this.markerLocations[index] = location;
-    }
+        this.markerLocations[index] = location;
+    };
     
     this.updateMarkerAt = function(index) {
         var point = map.locationPoint(this.markerLocations[index]),
@@ -213,7 +213,7 @@ function MarkerClip(map) {
         marker.id = map.parent.id+'-marker-'+createdMarkerCount;
         createdMarkerCount++;
         var px_size = size + 'px';
-	marker.pxsize = px_size
+        marker.pxsize = px_size;
         marker.style.width = px_size;
         marker.style.height = px_size;
         marker.style.margin = '0';
@@ -242,7 +242,7 @@ var state = {};
 state.lat = null;
 state.lon = null;
 state.pitch = 0.0;
-state.roll = 0.0
+state.roll = 0.0;
 state.heading = 0.0;
 state.waypoints = null;
 
@@ -278,9 +278,9 @@ function initMap() {
 
     var zoomSlider = document.getElementById('zoom');
     zoomSlider.onchange = function() {
-	var sliderProp = (zoomSlider.value - zoomSlider.min) / (zoomSlider.max - zoomSlider.min);
-	var targetZoom = sliderProp * 18.0; 
-	map.setZoom(targetZoom);
+        var sliderProp = (zoomSlider.value - zoomSlider.min) / (zoomSlider.max - zoomSlider.min);
+        var targetZoom = sliderProp * 18.0; 
+        map.setZoom(targetZoom);
     };
 }
 
@@ -288,24 +288,24 @@ function initMap() {
 function updateLinkStatus() {
     var now = (new Date()).getTime();
     if (!last_state_update_time) {
-	$("#t_link").html('<span class="link error">NO</span>');
+        $("#t_link").html('<span class="link error">NO</span>');
     } else if (now - last_state_update_time > 5000) {
-	$("#t_link").html('<span class="link error">TIMEOUT</span>');
+        $("#t_link").html('<span class="link error">TIMEOUT</span>');
     } else if (now - last_state_update_time > 1000) {
-	$("#t_link").html('<span class="link slow">SLOW</span>');
+        $("#t_link").html('<span class="link slow">SLOW</span>');
     } else {
-	$("#t_link").html('<span class="link ok">OK</span>');
+        $("#t_link").html('<span class="link ok">OK</span>');
     }
 }
 
 function updateState() {
     $.getJSON("data",
-	      function(data){
-		  state = data;
-		  updateTelemetryDisplay();
-		  updateMap();
-		  last_state_update_time = new Date().getTime();
-	      });
+              function(data){
+                  state = data;
+                  updateTelemetryDisplay();
+                  updateMap();
+                  last_state_update_time = new Date().getTime();
+              });
     updateLinkStatus();
 }
 
@@ -313,9 +313,9 @@ function updateState() {
 function updateMap() {
     var location = new MM.Location(state.lat, state.lon);
     if (!last_state_update_time) {
-	map.setCenter(location);
+        map.setCenter(location);
     } else {
-	mapPanner.setCenter(location);
+        mapPanner.setCenter(location);
     }
 }
 
@@ -331,15 +331,15 @@ function updateTelemetryDisplay() {
     now = new Date().getTime();
     $("#t_fps").html((1000.0 / (now - last_state_update_time)).toPrecision(3));
     if (state.gps_fix_type >= 3) {
-	$("#t_gps").html('<span class="ok">OK</span>');
+        $("#t_gps").html('<span class="ok">OK</span>');
     } else if (state.gps_fix_type == 2) {
-	$("#t_gps").html('<span class="slow">02</span>');
+        $("#t_gps").html('<span class="slow">02</span>');
     } else if (state.gps_fix_type <= 1) {
-	$("#t_gps").html('<span class="error">' + state.gps_fix_type + '</span>');
+        $("#t_gps").html('<span class="error">' + state.gps_fix_type + '</span>');
     }
     adi.setAttitude(state.pitch, state.roll);
     
-    rotate_drone(state.heading)
+    rotate_drone(state.heading);
     trailPlotter.update();
     marker_clip.setMarkerLocation(0, new MM.Location(state.lat, state.lon));
 }
@@ -348,11 +348,11 @@ function updateTelemetryDisplay() {
 function rotate_drone(deg){
     var rotate = "rotate(" + (deg) + "deg);";
     var tr = new Array(
-	"transform:" + rotate,
-	"-moz-transform:" + rotate,
-	"-webkit-transform:" + rotate,
-	"-ms-transform:" + rotate,
-	"-o-transform:" + rotate
+        "transform:" + rotate,
+        "-moz-transform:" + rotate,
+        "-webkit-transform:" + rotate,
+        "-ms-transform:" + rotate,
+        "-o-transform:" + rotate
     );
 
     var drone = document.getElementById("drone");
@@ -365,17 +365,18 @@ function updateLayer() {
     var layerNum = $(this).attr('value');
     console.log("Switching to layer " + layerNum);
     var bing_key = "Anmc0b2q6140lnPvAj5xANM1rvF1A4CvVtr6H2VJvQcdnDvc8NL-I2C49owIe9xC";
+    var style;
     if (layerNum == '1') {
-	var style = 'AerialWithLabels';
-	provider = new MM.BingProvider(bing_key, style, function(provider) { map_layer.setProvider(provider); });
+        style = 'AerialWithLabels';
+        provider = new MM.BingProvider(bing_key, style, function(provider) { map_layer.setProvider(provider); });
     } else if (layerNum == '2') {
-	var style = 'BirdseyeWithLabels';
-	provider = new MM.BingProvider(bing_key, style, function(provider) { map_layer.setProvider(provider); });
+        style = 'BirdseyeWithLabels';
+        provider = new MM.BingProvider(bing_key, style, function(provider) { map_layer.setProvider(provider); });
     } else if (layerNum == '3') {
-	var style = 'Road';
-	provider = new MM.BingProvider(bing_key, style, function(provider) { map_layer.setProvider(provider); });
+        style = 'Road';
+        provider = new MM.BingProvider(bing_key, style, function(provider) { map_layer.setProvider(provider); });
     } else if (layerNum == '4') {
-	provider = new MM.BlueMarbleProvider();
-	map_layer.setProvider(provider);
+        provider = new MM.BlueMarbleProvider();
+        map_layer.setProvider(provider);
     }
 }
