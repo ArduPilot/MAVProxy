f6e70ac53d39227d08cdea2c106b46882f5da867
Buzz <davidbuzz@gmail.com>
2012-08-16 23:10:42 +1000

    latest crap

--- mavgui/run_mavproxy_and_gui.sh
+++ mavgui/run_mavproxy_and_gui.sh
@@ -2,6 +2,7 @@
 echo "As the first parameter, please pass in the device name where your APM is connected ( ie the USB or Telemetry device )"
 mkdir ./GUI
 echo "module load netconsole" > ./GUI/mavinit.scr
+echo "module load mmap" >> ./GUI/mavinit.scr
 python2.7 mavproxy.py --master=$1 --out=127.0.0.1:6678 --aircraft=GUI & 
 cd mavgui
 python2.7 mavgui.py
--- run_mavproxy_and_udp_to_windows_gcs.sh
+++ run_mavproxy_and_udp_to_windows_gcs.sh
@@ -1,1 +1,3 @@
-python2.7 mavproxy.py --master=/dev/cu.usbserial-A700felb --out=10.11.12.2:14550 
+#python2.7 mavproxy.py --master=/dev/cu.usbserial-A700felb --out=10.11.12.2:14550 
+python2.7 mavproxy.py --master=/dev/cu.usbserial-A6007nZe  --out=10.11.12.2:14550 
+
