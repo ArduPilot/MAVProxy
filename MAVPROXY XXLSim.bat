9381018992a1aa605328be2dea760d79b82c8f46
crashmatt <uavflightdirector@gmail.com>
2013-04-20 09:30:20 +0200

    Added working batch files

---
+++ MAVPROXY XXLSim.bat
@@ -0,0 +1,2 @@
+Start mavproxy.py --master=COM1 --baudrate=57600 --target-system=55 --aircraft=XXXLsim --target-component=1 --source-system=253 --out=localhost:25999 --out=localhost:14550  --streamrate=1
+
---
+++ mavproxy xxxl USB0.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+python mavproxy.py --master=/dev/ttyUSB0 --baudrate=57600 target-system=55 --aircraft=XXXL --target-component=1 --source-system=252 --out=localhost:25995 --out=localhost:14550 --streamrate=1
+
+
+
