8a0821964bb071bf12a33787fb641f413df13451
Andrew Tridgell <tridge@samba.org>
2013-06-12 15:59:54 +1000

    import some ANUGA modules
    used for UTM calculations

---
+++ modules/lib/ANUGA/geo_reference.py
@@ -0,0 +1,469 @@
+"""
+This module adapted ANUGA
+https://anuga.anu.edu.au/
+
+"""
+
+
+#FIXME: Ensure that all attributes of a georef are treated everywhere
+#and unit test
+
+import types, sys
+import copy
+import numpy as num
+
+
+DEFAULT_ZONE = -1
+TITLE = '#geo reference' + "\n" # this title is referred to in the test format
+
+DEFAULT_PROJECTION = 'UTM'
+DEFAULT_DATUM = 'wgs84'
+DEFAULT_UNITS = 'm'
+DEFAULT_FALSE_EASTING = 500000
+DEFAULT_FALSE_NORTHING = 10000000    # Default for southern hemisphere
+
+
+##
+# @brief A class for ...
+class Geo_reference:
+    """
+    Attributes of the Geo_reference class:
+        .zone           The UTM zone (default is -1)
+        .false_easting  ??
+        .false_northing ??
+        .datum          The Datum used (default is wgs84)
+        .projection     The projection used (default is 'UTM')
+        .units          The units of measure used (default metres)
+        .xllcorner      The X coord of origin (default is 0.0 wrt UTM grid)
+        .yllcorner      The y coord of origin (default is 0.0 wrt UTM grid)
+        .is_absolute    ??
+
+    """
+
+    ##
+    # @brief Instantiate an instance of class Geo_reference.
+    # @param zone The UTM zone.
+    # @param xllcorner X coord of origin of georef.
+    # @param yllcorner Y coord of origin of georef.
+    # @param datum ??
+    # @param projection The projection used (default UTM).
+    # @param units Units used in measuring distance (default m).
+    # @param false_easting ??
+    # @param false_northing ??
+    # @param NetCDFObject NetCDF file *handle* to write to.
+    # @param ASCIIFile ASCII text file *handle* to write to.
+    # @param read_title Title of the georeference text.
+    def __init__(self,
+                 zone=DEFAULT_ZONE,
+                 xllcorner=0.0,
+                 yllcorner=0.0,
+                 datum=DEFAULT_DATUM,
+                 projection=DEFAULT_PROJECTION,
+                 units=DEFAULT_UNITS,
+                 false_easting=DEFAULT_FALSE_EASTING,
+                 false_northing=DEFAULT_FALSE_NORTHING,
+                 NetCDFObject=None,
+                 ASCIIFile=None,
+                 read_title=None):
+        """
+        input:
+        NetCDFObject - a handle to the netCDF file to be written to
+        ASCIIFile - a handle to the text file
+        read_title - the title of the georeference text, if it was read in.
+         If the function that calls this has already read the title line,
+         it can't unread it, so this info has to be passed.
+         If you know of a way to unread this info, then tell us.
+
+         Note, the text file only saves a sub set of the info the
+         points file does.  Currently the info not written in text
+         must be the default info, since ANUGA assumes it isn't
+         changing.
+        """
+
+        if zone is None:
+            zone = DEFAULT_ZONE
+        self.false_easting = int(false_easting)
+        self.false_northing = int(false_northing)
+        self.datum = datum
+        self.projection = projection
+        self.zone = int(zone)
+        self.units = units
+        self.xllcorner = float(xllcorner)
+        self.yllcorner = float(yllcorner)
+            
+        if NetCDFObject is not None:
+            self.read_NetCDF(NetCDFObject)
+
+        if ASCIIFile is not None:
+            self.read_ASCII(ASCIIFile, read_title=read_title)
+            
+        # Set flag for absolute points (used by get_absolute)    
+        self.absolute = num.allclose([self.xllcorner, self.yllcorner], 0)
+            
+
+    def get_xllcorner(self):
+        return self.xllcorner
+
+    ##
+    # @brief Get the Y coordinate of the origin of this georef.
+    def get_yllcorner(self):
+        return self.yllcorner
+
+    ##
+    # @brief Get the zone of this georef.
+    def get_zone(self):
+        return self.zone
+
+    ##
+    # @brief Write <something> to an open NetCDF file.
+    # @param outfile Handle to open NetCDF file.
+    def write_NetCDF(self, outfile):
+        outfile.xllcorner = self.xllcorner
+        outfile.yllcorner = self.yllcorner
+        outfile.zone = self.zone
+
+        outfile.false_easting = self.false_easting
+        outfile.false_northing = self.false_northing
+
+        outfile.datum = self.datum
+        outfile.projection = self.projection
+        outfile.units = self.units
+
+    ##
+    # @brief Read data from an open NetCDF file.
+    # @param infile Handle to open NetCDF file.
+    def read_NetCDF(self, infile):
+        self.xllcorner = float(infile.xllcorner[0])
+        self.yllcorner = float(infile.yllcorner[0])
+        self.zone = int(infile.zone[0])
+
+        try:
+            self.false_easting = int(infile.false_easting[0])
+            self.false_northing = int(infile.false_northing[0])
+
+            self.datum = infile.datum
+            self.projection = infile.projection
+            self.units = infile.units
+        except:
+            pass
+
+        if self.false_easting != DEFAULT_FALSE_EASTING:
+            print "WARNING: False easting of %f specified." % self.false_easting
+            print "Default false easting is %f." % DEFAULT_FALSE_EASTING
+            print "ANUGA does not correct for differences in False Eastings."
+
+        if self.false_northing != DEFAULT_FALSE_NORTHING:
+            print ("WARNING: False northing of %f specified."
+                   % self.false_northing)
+            print "Default false northing is %f." % DEFAULT_FALSE_NORTHING
+            print "ANUGA does not correct for differences in False Northings."
+
+        if self.datum.upper() != DEFAULT_DATUM.upper():
+            print "WARNING: Datum of %s specified." % self.datum
+            print "Default Datum is %s." % DEFAULT_DATUM
+            print "ANUGA does not correct for differences in datums."
+
+        if self.projection.upper() != DEFAULT_PROJECTION.upper():
+            print "WARNING: Projection of %s specified." % self.projection
+            print "Default Projection is %s." % DEFAULT_PROJECTION
+            print "ANUGA does not correct for differences in Projection."
+
+        if self.units.upper() != DEFAULT_UNITS.upper():
+            print "WARNING: Units of %s specified." % self.units
+            print "Default units is %s." % DEFAULT_UNITS
+            print "ANUGA does not correct for differences in units."
+
+################################################################################
+# ASCII files with geo-refs are currently not used
+################################################################################
+
+    ##
+    # @brief Write georef data to an open text file.
+    # @param fd Handle to open text file.
+    def write_ASCII(self, fd):
+        fd.write(TITLE)
+        fd.write(str(self.zone) + "\n")
+        fd.write(str(self.xllcorner) + "\n")
+        fd.write(str(self.yllcorner) + "\n")
+
+    ##
+    # @brief Read georef data from an open text file.
+    # @param fd Handle to open text file.
+    def read_ASCII(self, fd, read_title=None):
+        try:
+            if read_title == None:
+                read_title = fd.readline()     # remove the title line
+            if read_title[0:2].upper() != TITLE[0:2].upper():
+                msg = ('File error.  Expecting line: %s.  Got this line: %s'
+                       % (TITLE, read_title))
+                raise TitleError, msg
+            self.zone = int(fd.readline())
+            self.xllcorner = float(fd.readline())
+            self.yllcorner = float(fd.readline())
+        except SyntaxError:
+            msg = 'File error.  Got syntax error while parsing geo reference'
+            raise ParsingError, msg
+
+        # Fix some assertion failures
+        if isinstance(self.zone, num.ndarray) and self.zone.shape == ():
+            self.zone = self.zone[0]
+        if (isinstance(self.xllcorner, num.ndarray) and
+                self.xllcorner.shape == ()):
+            self.xllcorner = self.xllcorner[0]
+        if (isinstance(self.yllcorner, num.ndarray) and
+                self.yllcorner.shape == ()):
+            self.yllcorner = self.yllcorner[0]
+
+        assert (type(self.xllcorner) == types.FloatType)
+        assert (type(self.yllcorner) == types.FloatType)
+        assert (type(self.zone) == types.IntType)
+
+################################################################################
+
+    ##
+    # @brief Change points to be absolute wrt new georef 'points_geo_ref'.
+    # @param points The points to change.
+    # @param points_geo_ref The new georef to make points absolute wrt.
+    # @return The changed points.
+    # @note If 'points' is a list then a changed list is returned.
+    def change_points_geo_ref(self, points, points_geo_ref=None):
+        """Change the geo reference of a list or numeric array of points to
+        be this reference.(The reference used for this object)
+        If the points do not have a geo ref, assume 'absolute' values
+        """
+        import copy
+       
+        # remember if we got a list
+        is_list = isinstance(points, list)
+
+        points = ensure_numeric(points, num.float)
+
+        # sanity checks	
+        if len(points.shape) == 1:
+            #One point has been passed
+            msg = 'Single point must have two elements'
+            assert len(points) == 2, msg
+            points = num.reshape(points, (1,2))
+
+        msg = 'Points array must be two dimensional.\n'
+        msg += 'I got %d dimensions' %len(points.shape)
+        assert len(points.shape) == 2, msg
+
+        msg = 'Input must be an N x 2 array or list of (x,y) values. '
+        msg += 'I got an %d x %d array' %points.shape    
+        assert points.shape[1] == 2, msg                
+
+        # FIXME (Ole): Could also check if zone, xllcorner, yllcorner 
+        # are identical in the two geo refs.    
+        if points_geo_ref is not self:
+            # If georeferences are different
+            points = copy.copy(points) # Don't destroy input                    
+            if not points_geo_ref is None:
+                # Convert points to absolute coordinates
+                points[:,0] += points_geo_ref.xllcorner 
+                points[:,1] += points_geo_ref.yllcorner 
+        
+            # Make points relative to primary geo reference
+            points[:,0] -= self.xllcorner 
+            points[:,1] -= self.yllcorner
+
+        if is_list:
+            points = points.tolist()
+            
+        return points
+
+    def is_absolute(self):
+        """Return True if xllcorner==yllcorner==0 indicating that points
+        in question are absolute.
+        """
+        
+        # FIXME(Ole): It is unfortunate that decision about whether points
+        # are absolute or not lies with the georeference object. Ross pointed this out.
+        # Moreover, this little function is responsible for a large fraction of the time
+        # using in data fitting (something in like 40 - 50%.
+        # This was due to the repeated calls to allclose.
+        # With the flag method fitting is much faster (18 Mar 2009).
+
+        # FIXME(Ole): HACK to be able to reuse data already cached (18 Mar 2009). 
+        # Remove at some point
+        if not hasattr(self, 'absolute'):
+            self.absolute = num.allclose([self.xllcorner, self.yllcorner], 0)
+            
+        # Return absolute flag    
+        return self.absolute
+
+    def get_absolute(self, points):
+        """Given a set of points geo referenced to this instance,
+        return the points as absolute values.
+        """
+
+        # remember if we got a list
+        is_list = isinstance(points, list)
+
+        points = ensure_numeric(points, num.float)
+        if len(points.shape) == 1:
+            # One point has been passed
+            msg = 'Single point must have two elements'
+            if not len(points) == 2:
+                raise ShapeError, msg    
+
+
+        msg = 'Input must be an N x 2 array or list of (x,y) values. '
+        msg += 'I got an %d x %d array' %points.shape    
+        if not points.shape[1] == 2:
+            raise ShapeError, msg    
+            
+        
+        # Add geo ref to points
+        if not self.is_absolute():
+            points = copy.copy(points) # Don't destroy input                    
+            points[:,0] += self.xllcorner 
+            points[:,1] += self.yllcorner
+
+        
+        if is_list:
+            points = points.tolist()
+             
+        return points
+
+    ##
+    # @brief Convert points to relative measurement.
+    # @param points Points to convert to relative measurements.
+    # @return A set of points relative to the geo_reference instance.
+    def get_relative(self, points):
+        """Given a set of points in absolute UTM coordinates,
+        make them relative to this geo_reference instance,
+        return the points as relative values.
+
+        This is the inverse of get_absolute.
+        """
+
+        # remember if we got a list
+        is_list = isinstance(points, list)
+
+        points = ensure_numeric(points, num.float)
+        if len(points.shape) == 1:
+            #One point has been passed
+            msg = 'Single point must have two elements'
+            if not len(points) == 2:
+                raise ShapeError, msg    
+
+        if not points.shape[1] == 2:
+            msg = ('Input must be an N x 2 array or list of (x,y) values. '
+                   'I got an %d x %d array' % points.shape)
+            raise ShapeError, msg    
+
+        # Subtract geo ref from points
+        if not self.is_absolute():
+            points = copy.copy(points) # Don't destroy input                            
+            points[:,0] -= self.xllcorner 
+            points[:,1] -= self.yllcorner
+
+        if is_list:
+            points = points.tolist()
+             
+        return points
+
+    ##
+    # @brief ??
+    # @param other ??
+    def reconcile_zones(self, other):
+        if other is None:
+            other = Geo_reference()
+        if (self.zone == other.zone or
+            self.zone == DEFAULT_ZONE and
+            other.zone == DEFAULT_ZONE):
+            pass
+        elif self.zone == DEFAULT_ZONE:
+            self.zone = other.zone
+        elif other.zone == DEFAULT_ZONE:
+            other.zone = self.zone
+        else:
+            msg = ('Geospatial data must be in the same '
+                   'ZONE to allow reconciliation. I got zone %d and %d'
+                   % (self.zone, other.zone))
+            raise ANUGAError, msg
+
+    #def easting_northing2geo_reffed_point(self, x, y):
+    #    return [x-self.xllcorner, y - self.xllcorner]
+
+    #def easting_northing2geo_reffed_points(self, x, y):
+    #    return [x-self.xllcorner, y - self.xllcorner]
+
+    ##
+    # @brief Get origin of this geo_reference.
+    # @return (zone, xllcorner, yllcorner).
+    def get_origin(self):
+        return (self.zone, self.xllcorner, self.yllcorner)
+
+    ##
+    # @brief Get a string representation of this geo_reference instance.
+    def __repr__(self):
+        return ('(zone=%i easting=%f, northing=%f)'
+                % (self.zone, self.xllcorner, self.yllcorner))
+
+    ##
+    # @brief Compare two geo_reference instances.
+    # @param self This geo_reference instance.
+    # @param other Another geo_reference instance to compare against.
+    # @return 0 if instances have the same attributes, else 1.
+    # @note Attributes are: zone, xllcorner, yllcorner.
+    def __cmp__(self, other):
+        # FIXME (DSG) add a tolerence
+        if other is None:
+            return 1
+        cmp = 0
+        if not (self.xllcorner == self.xllcorner):
+            cmp = 1
+        if not (self.yllcorner == self.yllcorner):
+            cmp = 1
+        if not (self.zone == self.zone):
+            cmp = 1
+        return cmp
+
+
+##
+# @brief Write a geo_reference to a NetCDF file (usually SWW).
+# @param origin A georef instance or parameters to create a georef instance.
+# @param outfile Path to file to write.
+# @return A normalized geo_reference.
+def write_NetCDF_georeference(origin, outfile):
+    """Write georeference info to a netcdf file, usually sww.
+
+    The origin can be a georef instance or parameters for a geo_ref instance
+
+    outfile is the name of the file to be written to.
+    """
+
+    geo_ref = ensure_geo_reference(origin)
+    geo_ref.write_NetCDF(outfile)
+    return geo_ref
+
+
+##
+# @brief Convert an object to a georeference instance.
+# @param origin A georef instance or (zone, xllcorner, yllcorner)
+# @return A georef object, or None if 'origin' was None.
+def ensure_geo_reference(origin):
+    """
+    Given a list/tuple of zone, xllcorner and yllcorner of a geo-ref object,
+    return a geo ref object.
+
+    If the origin is None, return None, so calling this function doesn't
+    effect code logic
+    """
+
+    if isinstance(origin, Geo_reference):
+        geo_ref = origin
+    elif origin is None:
+        geo_ref = None
+    else:
+        geo_ref = apply(Geo_reference, origin)
+
+    return geo_ref
+
+
+#-----------------------------------------------------------------------
+
+if __name__ == "__main__":
+    pass
---
+++ modules/lib/ANUGA/redfearn.py
@@ -0,0 +1,243 @@
+"""
+This module adapted ANUGA
+https://anuga.anu.edu.au/
+
+------------
+
+Implementation of Redfearn's formula to compute UTM projections from latitude and longitude
+
+Based in part on spreadsheet
+www.icsm.gov.au/gda/gdatm/redfearn.xls
+downloaded from INTERGOVERNMENTAL COMMITTEE ON SURVEYING & MAPPING (ICSM)
+http://www.icsm.gov.au/icsm/
+
+"""
+from geo_reference import Geo_reference, DEFAULT_ZONE
+
+
+def degminsec2decimal_degrees(dd,mm,ss):
+    assert abs(mm) == mm
+    assert abs(ss) == ss    
+
+    if dd < 0:
+        sign = -1
+    else:
+        sign = 1
+    
+    return sign * (abs(dd) + mm/60. + ss/3600.)      
+    
+def decimal_degrees2degminsec(dec):
+
+    if dec < 0:
+        sign = -1
+    else:
+        sign = 1
+
+    dec = abs(dec)    
+    dd = int(dec)
+    f = dec-dd
+
+    mm = int(f*60)
+    ss = (f*60-mm)*60
+    
+    return sign*dd, mm, ss
+
+def redfearn(lat, lon, false_easting=None, false_northing=None,
+             zone=None, central_meridian=None, scale_factor=None):
+    """Compute UTM projection using Redfearn's formula
+
+    lat, lon is latitude and longitude in decimal degrees
+
+    If false easting and northing are specified they will override
+    the standard
+
+    If zone is specified reproject lat and long to specified zone instead of
+    standard zone
+
+    If meridian is specified, reproject lat and lon to that instead of zone. In this case
+    zone will be set to -1 to indicate non-UTM projection
+
+    Note that zone and meridian cannot both be specifed
+    """
+
+
+    from math import pi, sqrt, sin, cos, tan
+    
+
+
+    #GDA Specifications
+    a = 6378137.0                       #Semi major axis
+    inverse_flattening = 298.257222101  #1/f
+    if scale_factor is None:
+        K0 = 0.9996                         #Central scale factor    
+    else:
+        K0 = scale_factor
+    #print 'scale', K0
+    zone_width = 6                      #Degrees
+
+    longitude_of_central_meridian_zone0 = -183    
+    longitude_of_western_edge_zone0 = -186
+
+    if false_easting is None:
+        false_easting = 500000
+
+    if false_northing is None:
+        if lat < 0:
+            false_northing = 10000000  #Southern hemisphere
+        else:
+            false_northing = 0         #Northern hemisphere)
+        
+    
+    #Derived constants
+    f = 1.0/inverse_flattening
+    b = a*(1-f)       #Semi minor axis
+
+    e2 = 2*f - f*f#    = f*(2-f) = (a^2-b^2/a^2   #Eccentricity
+    e = sqrt(e2)
+    e2_ = e2/(1-e2)   # = (a^2-b^2)/b^2 #Second eccentricity
+    e_ = sqrt(e2_)
+    e4 = e2*e2
+    e6 = e2*e4
+
+    #Foot point latitude
+    n = (a-b)/(a+b) #Same as e2 - why ?
+    n2 = n*n
+    n3 = n*n2
+    n4 = n2*n2
+
+    G = a*(1-n)*(1-n2)*(1+9*n2/4+225*n4/64)*pi/180
+
+
+    phi = lat*pi/180     #Convert latitude to radians
+
+    sinphi = sin(phi)   
+    sin2phi = sin(2*phi)
+    sin4phi = sin(4*phi)
+    sin6phi = sin(6*phi)
+
+    cosphi = cos(phi)
+    cosphi2 = cosphi*cosphi
+    cosphi3 = cosphi*cosphi2
+    cosphi4 = cosphi2*cosphi2
+    cosphi5 = cosphi*cosphi4    
+    cosphi6 = cosphi2*cosphi4
+    cosphi7 = cosphi*cosphi6
+    cosphi8 = cosphi4*cosphi4        
+
+    t = tan(phi)
+    t2 = t*t
+    t4 = t2*t2
+    t6 = t2*t4
+    
+    #Radius of Curvature
+    rho = a*(1-e2)/(1-e2*sinphi*sinphi)**1.5
+    nu = a/(1-e2*sinphi*sinphi)**0.5
+    psi = nu/rho
+    psi2 = psi*psi
+    psi3 = psi*psi2
+    psi4 = psi2*psi2
+
+
+
+    #Meridian distance
+
+    A0 = 1 - e2/4 - 3*e4/64 - 5*e6/256
+    A2 = 3.0/8*(e2+e4/4+15*e6/128)
+    A4 = 15.0/256*(e4+3*e6/4)
+    A6 = 35*e6/3072
+    
+    term1 = a*A0*phi
+    term2 = -a*A2*sin2phi
+    term3 = a*A4*sin4phi
+    term4 = -a*A6*sin6phi
+
+    m = term1 + term2 + term3 + term4 #OK
+
+    if zone is not None and central_meridian is not None:
+        msg = 'You specified both zone and central_meridian. Provide only one of them'
+        raise Exception, msg
+    
+    # Zone
+    if zone is None:
+        zone = int((lon - longitude_of_western_edge_zone0)/zone_width)
+
+    # Central meridian
+    if central_meridian is None:
+        central_meridian = zone*zone_width+longitude_of_central_meridian_zone0
+    else:
+        zone = -1
+
+    omega = (lon-central_meridian)*pi/180 #Relative longitude (radians)
+    omega2 = omega*omega
+    omega3 = omega*omega2
+    omega4 = omega2*omega2
+    omega5 = omega*omega4
+    omega6 = omega3*omega3
+    omega7 = omega*omega6
+    omega8 = omega4*omega4
+     
+    #Northing
+    term1 = nu*sinphi*cosphi*omega2/2  
+    term2 = nu*sinphi*cosphi3*(4*psi2+psi-t2)*omega4/24
+    term3 = nu*sinphi*cosphi5*\
+            (8*psi4*(11-24*t2)-28*psi3*(1-6*t2)+\
+             psi2*(1-32*t2)-psi*2*t2+t4-t2)*omega6/720
+    term4 = nu*sinphi*cosphi7*(1385-3111*t2+543*t4-t6)*omega8/40320
+    northing = false_northing + K0*(m + term1 + term2 + term3 + term4)
+
+    #Easting
+    term1 = nu*omega*cosphi
+    term2 = nu*cosphi3*(psi-t2)*omega3/6
+    term3 = nu*cosphi5*(4*psi3*(1-6*t2)+psi2*(1+8*t2)-2*psi*t2+t4)*omega5/120
+    term4 = nu*cosphi7*(61-479*t2+179*t4-t6)*omega7/5040
+    easting = false_easting + K0*(term1 + term2 + term3 + term4)
+    
+    return zone, easting, northing
+
+
+
+def convert_from_latlon_to_utm(points=None,
+                               latitudes=None,
+                               longitudes=None,
+                               false_easting=None,
+                               false_northing=None):
+    """Convert latitude and longitude data to UTM as a list of coordinates.
+
+
+    Input
+
+    points: list of points given in decimal degrees (latitude, longitude) or
+    latitudes: list of latitudes   and
+    longitudes: list of longitudes 
+    false_easting (optional)
+    false_northing (optional)
+
+    Output
+
+    points: List of converted points
+    zone:   Common UTM zone for converted points
+
+
+    Notes
+
+    Assume the false_easting and false_northing are the same for each list.
+    If points end up in different UTM zones, an ANUGAerror is thrown.    
+    """
+
+    old_geo = Geo_reference()    
+    utm_points = []
+    if points == None:
+        assert len(latitudes) == len(longitudes)
+        points =  map(None, latitudes, longitudes)
+        
+    for point in points:
+        
+        zone, easting, northing = redfearn(float(point[0]),
+                                           float(point[1]),
+                                           false_easting=false_easting,
+                                           false_northing=false_northing)
+        new_geo = Geo_reference(zone)
+        old_geo.reconcile_zones(new_geo)        
+        utm_points.append([easting, northing])
+
+    return utm_points, old_geo.get_zone()
