dbf87ec254db5302ab16e88d1baa94ab38887da4
Stephen Dade <stephen_dade@hotmail.com>
2013-08-27 23:02:33 +1000

    SRTM - GA submodule now using online database

--- modules/mavproxy_map/GAreader.py
+++ modules/mavproxy_map/GAreader.py
@@ -2,158 +2,109 @@
 '''
 Module to read DTM files published by Geoscience Australia
 Written by Stephen Dade (stephen_dade@hotmail.com
+Chunks are downloaded in 240x240 pixel tiffs, 5x5min (9.3x9.3km) geo area
+Thus each pixel is ~39x39m
+File format will be 115124N145123W.tiff, where the top left corner is 115.123N, 145.123W
 '''
 
 import os
-import sys
+import threading
+import time
+import urllib2
+import Image
 
 import numpy
 
-
+class downloadThread (threading.Thread):
+    '''simple download class thread'''
+    def __init__(self, name, site, file):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.site = site
+        self.file = file
+    def run(self):
+        print "Starting " + self.name + "\n"
+        with open(self.file, 'wb+') as f:
+            try:
+                f.write(urllib2.urlopen(self.site).read())
+            except urllib2.HTTPError:
+                print "Download fail"
+        print "Downloaded " + self.name
+            
 class ERMap:
     '''Class to read GA files'''
-    def __init__(self):
-        self.header = None
-        self.data = None
-        self.startlongitude = 0
-        self.startlatitude = 0
-        self.endlongitude = 0
-        self.endlatitude = 0
-        self.deltalongitude = 0
-        self.deltalatitude = 0
+    def __init__(self, cachedir=None, offline=0):
+    #set up the cache directory
+        if cachedir is None:
+            try:
+                cachedir = os.path.join(os.environ['HOME'], '.tilecache/GeoAus')
+            except Exception:
+                #cachedir = os.path.join(tempfile.gettempdir(), 'GeoAus')
+                cachedir = "C:\GAtmp"
 
-    def read_ermapper(self, ifile):
-        '''Read in a DEM file and associated .ers file'''
-        ers_index = ifile.find('.ers')
-        if ers_index > 0:
-            data_file = ifile[0:ers_index]
-            header_file = ifile
-        else:
-            data_file = ifile
-            header_file = ifile + '.ers'
-        
-        self.header = self.read_ermapper_header(header_file)
-
-        nroflines = int(self.header['nroflines'])
-        nrofcellsperlines = int(self.header['nrofcellsperline'])
-        self.data = self.read_ermapper_data(data_file, offset=int(self.header['headeroffset']))
-        self.data = numpy.reshape(self.data,(nroflines,nrofcellsperlines))
-
-        longy =  numpy.fromstring(self.getHeaderParam('longitude'), sep=':')
-        latty =  numpy.fromstring(self.getHeaderParam('latitude'), sep=':')
-
-        self.deltalatitude = float(self.header['ydimension'])
-        self.deltalongitude = float(self.header['xdimension'])
-
-        if longy[0] < 0:
-            self.startlongitude = longy[0]+-((longy[1]/60)+(longy[2]/3600))
-            self.endlongitude = self.startlongitude - int(self.header['nrofcellsperline'])*self.deltalongitude
-        else:
-            self.startlongitude = longy[0]+(longy[1]/60)+(longy[2]/3600)
-            self.endlongitude = self.startlongitude + int(self.header['nrofcellsperline'])*self.deltalongitude
-        if latty[0] < 0:
-            self.startlatitude = latty[0]-((latty[1]/60)+(latty[2]/3600))
-            self.endlatitude = self.startlatitude - int(self.header['nroflines'])*self.deltalatitude
-        else:
-            self.startlatitude = latty[0]+(latty[1]/60)+(latty[2]/3600)
-            self.endlatitude = self.startlatitude + int(self.header['nroflines'])*self.deltalatitude
-
-    def read_ermapper_header(self, ifile):
-        # function for reading an ERMapper header from file
-        header = {}
-
-        fid = open(ifile,'rt')
-        header_string = fid.readlines()
-        fid.close()
-
-        for line in header_string:
-            if line.find('=') > 0:
-                tmp_string = line.strip().split('=')
-                header[tmp_string[0].strip().lower()]= tmp_string[1].strip()
-
-        return header                      
-
-    def read_ermapper_data(self, ifile, data_format = numpy.float32, offset=0):
-        # open input file in a binary format and read the input string
-        fid = open(ifile,'rb')
-        if offset != 0:
-            fid.seek(offset)
-        input_string = fid.read()
-        fid.close()
-
-        # convert input string to required format (Note default format is numpy.float32)
-        grid_as_float = numpy.fromstring(input_string,data_format)
-        return grid_as_float
-
-    def getHeaderParam(self, key):
-         '''Find a parameter in the associated .ers file'''
-         return self.header[key]
-
-    def printBoundingBox(self):
-        '''Print the bounding box that this DEM covers'''
-        print "Bounding Latitude: "
-        print self.startlatitude
-        print self.endlatitude
-
-        print "Bounding Longitude: "
-        print self.startlongitude
-        print self.endlongitude
-
-    def getPercentBlank(self):
-        '''Print how many null cells are in the DEM - Quality measure'''
-        blank = 0
-        nonblank = 0
-        for x in self.data.flat:
-            if x == -99999.0:
-                blank = blank + 1
-            else:
-                nonblank = nonblank + 1
-
-        print "Blank tiles =  ", blank, "out of ", (nonblank+blank)
+        self.offline = offline
+        self.first_failure = False
+        self.server = "http://www.ga.gov.au/gisimg/services/topography/dem_1s/ImageServer/WMSServer?"
+        self.cachedir = cachedir
+        self.imgpixelsize = 240
+        self.imggeosize = 0.05
+        self.downloadThreads = []
+        '''print "SRTMDownloader - server= %s, directory=%s." % \
+        (self.server, self.directory)'''
+        if not os.path.exists(cachedir):
+            mp_util.mkdir_p(cachedir)
 
     def getAltitudeAtPoint(self, latty, longy):
         '''Return the altitude at a particular long/lat'''
-        #check the bounds
-        if self.startlongitude > 0 and (longy < self.startlongitude or longy > self.endlongitude):
-            return -1
-        if self.startlongitude < 0 and (longy > self.startlongitude or longy < self.endlongitude):
-            return -1
-        if self.startlatitude > 0 and (latty < self.startlatitude or longy > self.endlatitude):
-            return -1
-        if self.startlatitude < 0 and (latty > self.startlatitude or longy < self.endlatitude):
-            return -1
-
-        x = numpy.abs((latty - self.startlatitude)/self.deltalatitude)
-        y = numpy.abs((longy - self.startlongitude)/self.deltalongitude)
-
-        #do some interpolation
+        #Check we're actually in Australia:
+        if not (latty < -10 and latty > -40 and longy > 113 and longy < 154):
+            print "Outside GeoAustralia DTED range"
+            return 0
+        
+        #find out which chunk of data has this point 115124N145123W.tiff
         # print "x,y", x, y
+        x_id = self.positionToTile(latty, 1)
+        y_id = self.positionToTile(longy, 1)
+        data_chunk = "" + str(x_id) + "N" + str(y_id) + "W.tiff"
+        
+        #does this file already exist in the cache?
+        try:
+            data = open(os.path.join(self.cachedir, data_chunk), 'r')
+        except IOError:
+            #if it's not in the cache, make a new thread and download
+            print "Need to download " + data_chunk
+            if self.offline == 0:
+                self.downloadFile(x_id, y_id)
+            return -1
+        
+        #open up the file
+        #print "Opening " + str(os.path.join(self.cachedir, data_chunk))
+        im = Image.open(os.path.join(self.cachedir, data_chunk))
+        #print im.format, im.size, im.mode
+        pixelMap = im.load() #create the pixel map
+        
+        #figure out pixel indexes        
+        x = ((latty - self.positionToTile(latty, 0))/self.imggeosize) * self.imgpixelsize
+        y = ((longy - self.positionToTile(longy, 0))/self.imggeosize) * self.imgpixelsize
+        #print "long, lat" , latty, longy
+        #print "x,y ", x, y
         x_int = int(x)
         x_frac = x - int(x)
         y_int = int(y)
         y_frac = y - int(y)
+        
         #print "frac", x_int, x_frac, y_int, y_frac
-        value00 = self.data[x_int, y_int]
-        value10 = self.data[x_int+1, y_int]
-        value01 = self.data[x_int, y_int+1]
-        value11 = self.data[x_int+1, y_int+1]
+        value00 = (pixelMap[x_int, y_int])
+        value10 = (pixelMap[x_int+1, y_int])
+        value01 = (pixelMap[x_int, y_int+1])
+        value11 = (pixelMap[x_int+1, y_int+1])
         #print "values ", value00, value10, value01, value11
-
-        #check for null values
-        if value00 == -99999:
-            value00 = 0
-        if value10 == -99999:
-            value10 = 0
-        if value01 == -99999:
-            value01 = 0
-        if value11 == -99999:
-            value11 = 0
 
         value1 = self._avg(value00, value10, x_frac)
         value2 = self._avg(value01, value11, x_frac)
         value  = self._avg(value1,  value2, y_frac)
 
-        return value
+        return numpy.round(value, 2)
 
     @staticmethod
     def _avg(value1, value2, weight):
@@ -166,19 +117,38 @@ class ERMap:
             return value1
         return value2 * weight + value1 * (1 - weight)
 
+    def downloadFile(self, x_int, y_int):
+        '''Downloads the file from the GeoScience Australia servers in
+        a new thread'''
+        for curDown in self.downloadThreads:
+            if curDown == str(x_int) + ", " + str(y_int):
+                return
+        #http://www.ga.gov.au/gisimg/services/topography/dem_1s/ImageServer/WMSServer?
+        #request=GetMap&format=image/tiff&crs=CRS:84&width=640&height=480&bbox=151.666,-26.666,152.0,-26.5 
+        webaddress = self.server + "request=GetMap&format=image/tiff&crs=CRS:84&width=" + str(self.imgpixelsize) + "&height=" + str(self.imgpixelsize)
+        webaddress += "&bbox=" + str(float(y_int)/1000) + "," + str(float(x_int)/1000) + "," + str((float(y_int)/1000)+self.imggeosize) + "," + str((float(x_int)/1000)+self.imggeosize)
+        filename = os.path.join(self.cachedir, "" + str(x_int) + "N" + str(y_int) + "W.tiff")
+        thread1 = downloadThread(str(x_int) + ", " + str(y_int), webaddress, filename)
+        self.downloadThreads.append(thread1)
+        print "Site is " + webaddress
+        print "Downloading " + filename
+        thread1.start()
+        
+    def positionToTile(self, posn, tilecoords):
+        roundpos = (numpy.floor(posn / self.imggeosize) * self.imggeosize)
+        if tilecoords:
+            return int(roundpos*1000)
+        else:
+            return roundpos
 
 
 if __name__ == '__main__':
 
-    print "./Canberra/GSNSW_P756demg"
     mappy = ERMap()
-    mappy.read_ermapper(os.path.join(os.environ['HOME'], './Documents/Elevation/Canberra/GSNSW_P756demg'))
-
-    #print some header data   
-    mappy.printBoundingBox()
 
     #get a measure of data quality
     #mappy.getPercentBlank()
+    print "Note that script will need to be run twice to download and read cached tiles"
 
     #test the altitude (around Canberra):
     alt = mappy.getAltitudeAtPoint(-35.274411, 149.097504)
