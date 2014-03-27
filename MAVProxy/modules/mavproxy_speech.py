0bf1d07db4169e4887eeca4aca91da024c5dbe3f
Andrew Tridgell <tridge@samba.org>
2014-03-28 07:55:08 +1100

    speech: convert module to new style

--- MAVProxy/modules/mavproxy_speech.py
+++ MAVProxy/modules/mavproxy_speech.py
@@ -2,52 +2,50 @@
 '''tune command handling'''
 
 import time, os
+from MAVProxy.modules.lib import mp_module
 
-def name():
-    '''return module name'''
-    return "speech"
+class SpeechModule(mp_module.MPModule):
+    def __init__(self, mpstate):
+        super(SpeechModule, self).__init__(mpstate, "speech", "speech output")
+        self.mpstate.functions.say = self.say
+        self.settings.append(('speech', int, 1))
+        self.kill_speech_dispatcher()
+        self.say('')
 
-def description():
-    '''return module description'''
-    return "speech output handling"
+    def kill_speech_dispatcher(self):
+        '''kill speech dispatcher processs'''
+        pidpath = os.path.join(os.environ['HOME'], '.speech-dispatcher',
+                               'pid', 'speech-dispatcher.pid')
+        if os.path.exists(pidpath):
+            try:
+                import signal
+                pid = int(open(pidpath).read())
+                if pid > 1 and os.kill(pid, 0) is None:
+                    print("Killing speech dispatcher pid %u" % pid)
+                    os.kill(pid, signal.SIGINT)
+                    time.sleep(1)
+            except Exception as e:
+                pass
 
-def kill_speech_dispatcher():
-    '''kill speech dispatcher processs'''
-    pidpath = os.path.join(os.environ['HOME'], '.speech-dispatcher', 'pid', 'speech-dispatcher.pid')
-    if os.path.exists(pidpath):
-        try:
-            import signal
-            pid = int(open(pidpath).read())
-            if pid > 1 and os.kill(pid, 0) is None:
-                print("Killing speech dispatcher pid %u" % pid)
-                os.kill(pid, signal.SIGINT)
-                time.sleep(1)
-        except Exception as e:
-            pass
 
-def init(_mpstate):
+    def unload(self):
+        '''unload module'''
+        self.kill_speech_dispatcher()
+
+    def say(self, text, priority='important'):
+        '''speak some text'''
+        ''' http://cvs.freebsoft.org/doc/speechd/ssip.html see 4.3.1 for priorities'''
+        self.console.writeln(text)
+        if self.settings.speech:
+            import speechd
+            self.speech = speechd.SSIPClient('MAVProxy%u' % os.getpid())
+            self.speech.set_output_module('festival')
+            self.speech.set_language('en')
+            self.speech.set_priority(priority)
+            self.speech.set_punctuation(speechd.PunctuationMode.SOME)
+            self.speech.speak(text)
+            self.speech.close()
+
+def init(mpstate):
     '''initialise module'''
-    global mpstate
-    mpstate = _mpstate
-    mpstate.functions.say = say
-    mpstate.settings.append(('speech', int, 1))
-    kill_speech_dispatcher()
-    say('')
-
-def unload():
-    '''unload module'''
-    kill_speech_dispatcher()
-
-def say(text, priority='important'):
-    '''speak some text'''
-    ''' http://cvs.freebsoft.org/doc/speechd/ssip.html see 4.3.1 for priorities'''
-    mpstate.console.writeln(text)
-    if mpstate.settings.speech:
-        import speechd
-        mpstate.status.speech = speechd.SSIPClient('MAVProxy%u' % os.getpid())
-        mpstate.status.speech.set_output_module('festival')
-        mpstate.status.speech.set_language('en')
-        mpstate.status.speech.set_priority(priority)
-        mpstate.status.speech.set_punctuation(speechd.PunctuationMode.SOME)
-        mpstate.status.speech.speak(text)
-        mpstate.status.speech.close()
+    return SpeechModule(mpstate)
