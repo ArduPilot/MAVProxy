0ef487756d2d637e702ae43fa04451adf800694e
Andrew Tridgell <tridge@samba.org>
2014-04-18 09:55:35 +1000

    mp_settings: restructure ready for graphical settings

--- MAVProxy/modules/lib/mp_settings.py
+++ MAVProxy/modules/lib/mp_settings.py
@@ -1,34 +1,59 @@
 #!/usr/bin/env python
 '''settings object for MAVProxy modules'''
 
+class MPSetting:
+    def __init__(self, name, type, default, label=None):
+        self.name = name
+        self.type = type
+        self.default = default
+        self.label = label
+        self.value = default
+
 class MPSettings(object):
     def __init__(self, vars):
-        self.vars = vars
-        for var in vars:
-            (v,t,d) = var
-            self.set(v, d)
+        self._vars = {}
+        for v in vars:
+            self.append(v)
 
-    def append(self, var):
+    def append(self, v):
         '''add a new setting'''
-        (v,t,d) = var
-        self.vars.append(var)
-        self.set(v, d)
+        if isinstance(v, MPSetting):
+            setting = v
+        else:
+            (name,type,default) = v
+            setting = MPSetting(name, type, default)
+        self._vars[setting.name] = setting
 
-    def set(self, vname, value):
-        '''set a setting'''
-        if value is None or value == 'None':
-            setattr(self, vname, None)
+    def __getattr__(self, name):
+        if name == '_vars':
+            return self._vars
+        if name in self._vars:
+            return self._vars[name].value
+        raise AttributeError
+
+    def __setattr__(self, name, value):
+        if name == '_vars':
+            self.__dict__[name] = value
             return
-        for (v,t,d) in sorted(self.vars):
-            if v == vname:
-                try:
-                    value = t(value)
-                except:
-                    print("Unable to convert %s to type %s" % (value, t))
-                    return
-                setattr(self, vname, value)
+        if name in self._vars:
+            self._vars[name].value = value
+            return
+        raise AttributeError
+
+    def set(self, name, value):
+        '''set a setting'''
+        if not name in self._vars:
+            raise AttributeError
+        setting = self._vars[name]
+        if value == 'None' and setting.default is None:
+            value = None
+        if value is not None:
+            try:
+                value = setting.type(value)
+            except:
+                print("Unable to convert %s to type %s" % (value, setting.type))
                 return
-        print("Invalid setting %s" % vname)
+        setting.value = value
 
     def show(self, v):
         '''show settings'''
@@ -36,15 +61,12 @@ class MPSettings(object):
 
     def show_all(self):
         '''show all settings'''
-        for (v,t,d) in sorted(self.vars):
-            self.show(v)
+        for setting in sorted(self._vars):
+            self.show(setting)
 
     def list(self):
         '''list all settings'''
-        ret = []
-        for (v,t,d) in self.vars:
-            ret.append(v)
-        return ret
+        return self._vars.keys()
 
     def completion(self, text):
         '''completion function for cmdline completion'''
