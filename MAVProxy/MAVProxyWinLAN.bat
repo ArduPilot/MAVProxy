c4f3e928dc9b598e9e42ac831d3a253d4385958c
Stephen Dade <stephen_dade@hotmail.com>
2015-04-03 16:43:16 +1100

    Windows: Fixed up build and install scripts

--- MAVProxy/MAVProxyWinLAN.bat
+++ MAVProxy/MAVProxyWinLAN.bat
@@ -1,8 +1,4 @@
-setlocal
-
-SET PATH=%PATH%;..\mavlink;..\mavlink\pymavlink\examples
-SET PYTHONPATH=%PYTHONPATH%;C:\Python27\Lib;C:\Python27\Lib\site-packages;.\MAVProxy\modules;.\MAVProxy\modules\lib
 cd ..\
-.\MAVProxy\mavproxy.py --master=192.168.1.6:14550
-
+python setup.py build install --user
+python .\MAVProxy\mavproxy.py --master=192.168.1.6:14550 --console
 pause
