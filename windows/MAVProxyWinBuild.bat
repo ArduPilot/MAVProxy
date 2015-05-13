9356b5da268eaefa98048a4c1c1d7acad4ac1831
Stephen Dade <stephen_dade@hotmail.com>
2015-05-13 22:39:16 +1000

    Automated the Windows build script futher

--- windows/MAVProxyWinBuild.bat
+++ windows/MAVProxyWinBuild.bat
@@ -1,7 +1,26 @@
 rem build the standalone MAVProxy.exe for Windows.
 rem This assumes Python is installed in C:\Python27
+SETLOCAL enableextensions
+
+rem get the version
+for /f "tokens=*" %%a in (
+ 'python returnVersion.py'
+ ) do (
+ set VERSION=%%a
+ )
+
+
+rem -----Upgrade pymavlink if needed-----
+C:\Python27\Scripts\pip install pymavlink -U
+
+rem -----Build MAVProxy-----
 cd ..\
 python setup.py build install --user
 cd .\MAVProxy
 C:\Python27\Scripts\pyinstaller --clean ..\windows\mavproxy.spec
+
+rem -----Build the Installer-----
+cd  ..\windows\
+"C:\Program Files (x86)\Inno Setup 5\ISCC.exe" /dMyAppVersion=%VERSION% -compile mavproxy.iss
+
 pause
