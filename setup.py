dd9645893aa5d5907baa632eb72a51d6f0fe820b
Andrew Tridgell <andrew@tridgell.net>
2015-09-29 13:39:57 +1000

    removed lxml dependency
    the lxml dependency is not satisfied on Mint by python-lxml, so only
    soln is to remove it. Sigh

--- setup.py
+++ setup.py
@@ -42,8 +42,7 @@ on how to use MAVProxy.''',
       # use the system versions of these dependencies, so it tries to download and install
       # large numbers of modules like numpy etc which may be already installed
       install_requires=['pymavlink>=1.1.50',
-                        'pyserial',
-                        'lxml'],
+                        'pyserial'],
       scripts=['MAVProxy/mavproxy.py',
                'MAVProxy/tools/mavflightview.py',
                'MAVProxy/tools/MAVExplorer.py',
