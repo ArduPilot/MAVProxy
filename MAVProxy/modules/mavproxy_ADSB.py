081a0a81d85df4145869901ec480320307f498ec
uas <uas@GCS-5>
2015-12-03 23:15:52 +1030

    ADSB: refined threat detection and changed default settings to hide threat radius

--- MAVProxy/modules/mavproxy_ADSB.py
+++ MAVProxy/modules/mavproxy_ADSB.py
@@ -1,4 +1,8 @@
-'''Support for ADS-B data'''
+'''
+Support for ADS-B data
+Samuel Dudley
+Dec 2015
+'''
 
 import time
 from math import *
@@ -7,14 +11,17 @@ from MAVProxy.modules.lib import mp_module
 from MAVProxy.modules.mavproxy_map import mp_slipmap
 from MAVProxy.modules.lib import mp_settings
 from MAVProxy.modules.lib.mp_menu import * #popup menus
-from pymavlink import mavextra
+from pymavlink import mavutil
 
 
 class ADSBVehicle(object):
+    '''a generic ADS-B threat'''
     def __init__(self, id, state):
         self.id = id
         self.state = state
-        self.icon = 'greenplane'# use plane icon for now
+        self.vehicle_colour = 'green' #use plane icon for now
+        self.vehicle_type = 'plane'
+        self.icon = self.vehicle_colour+self.vehicle_type+'.png'
         self.update_time = time.time()
         self.is_evading_threat = False
         self.v_distance = None
@@ -30,6 +37,7 @@ class ADSBModule(mp_module.MPModule):
     def __init__(self, mpstate):
         super(ADSBModule, self).__init__(mpstate, "ADSB", "ADS-B data support")
         self.threat_vehicles = {}
+        self.active_threats = [] #holds all threats the vehicle is evading
         
         self.add_command('ADSB', self.cmd_ADSB, ["ADSB control",
                                                  "<status>", 
@@ -37,10 +45,10 @@ class ADSBModule(mp_module.MPModule):
         
         self.ADSB_settings = mp_settings.MPSettings([ ("timeout", int, 10), #seconds
                                                       ("threat_radius", int, 200), #meters
-                                                      ("show_threat_radius", bool, True),
+                                                      ("show_threat_radius", bool, False),
                                                       ("threat_radius_clear_multiplier", int, 2), #threat_radius_clear = threat_radius*threat_radius_clear_multiplier
-                                                      ("show_threat_radius_clear", bool, True)]) 
-        
+                                                      ("show_threat_radius_clear", bool, False)]) 
+        self.theat_detection_timer = mavutil.periodic_event(2)
         
     def cmd_ADSB(self, args):
         '''ADSB command parser'''
@@ -49,21 +57,35 @@ class ADSBModule(mp_module.MPModule):
             print(usage)
             return
         if args[0] == "status":
-            print("threat count: %u" % (len(self.threat_vehicles)))
+            print("total threat count: %u  active threat count: %u" % (len(self.threat_vehicles), len(self.active_threats)))
+            
             for id in self.threat_vehicles.keys():
-                print("id: %s  distance: %.2f callsign: %s  tslc: %u  lat: %.5f  lon: %.5f  alt: %.2f  cog: %u" % (id,
-                                                                                   self.threat_vehicles[id].distance,
-                                                                                   self.threat_vehicles[id].state['callsign'],
-                                                                                   self.threat_vehicles[id].state['tslc'],     
-                                                                                   self.threat_vehicles[id].state['lat'] * 1e-7,
-                                                                                   self.threat_vehicles[id].state['lon'] * 1e-7,
-                                                                                   self.threat_vehicles[id].state['altitude'],
-                                                                                   self.threat_vehicles[id].state['heading']))
+                print("id: %s  distance: %.2f m  callsign: %s  alt: %.2f" % (id,
+                                                                             self.threat_vehicles[id].distance,
+                                                                             self.threat_vehicles[id].state['callsign'],
+                                                                             self.threat_vehicles[id].state['altitude']))
         elif args[0] == "set":
             self.ADSB_settings.command(args[1:])
         else:
             print(usage)
-            
+    
+    def perform_threat_detection(self):
+        '''determine threats'''
+        #TODO: perform more advanced threat detection
+        threat_radius_clear = self.ADSB_settings.threat_radius*self.ADSB_settings.threat_radius_clear_multiplier
+        
+        for id in self.threat_vehicles.keys():
+            if self.threat_vehicles[id].distance is not None:
+                if self.threat_vehicles[id].distance <= self.ADSB_settings.threat_radius and not self.threat_vehicles[id].is_evading_threat:
+                    #if the threat is in the threat radius and not currently known to the module...
+                    self.threat_vehicles[id].is_evading_threat = True #set flag to action threat
+                    
+                if self.threat_vehicles[id].distance > threat_radius_clear and self.threat_vehicles[id].is_evading_threat:
+                    #if the threat is known to the module and outside the threat clear radius...
+                    self.threat_vehicles[id].is_evading_threat = False #clear flag to action threat
+                    
+        self.active_threats = [id for id in self.threat_vehicles.keys() if self.threat_vehicles[id].is_evading_threat]
+    
     def update_threat_distances(self, latlonalt):
         '''update the distance between threats and vehicle'''
         for id in self.threat_vehicles.keys():
@@ -81,6 +103,7 @@ class ADSBModule(mp_module.MPModule):
         '''get the horizontal distance between threat and vehicle'''
         (lat1, lon1, alt1) = latlonalt1
         (lat2, lon2, alt2) = latlonalt2
+        
         lat1 = radians(lat1)
         lon1 = radians(lon1)
         lat2 = radians(lat2)
@@ -89,6 +112,7 @@ class ADSBModule(mp_module.MPModule):
         dLat = lat2 - lat1
         dLon = lon2 - lon1
         
+        #math as per mavextra.distance_two()
         a = sin(0.5*dLat)**2 + sin(0.5*dLon)**2 * cos(lat1) * cos(lat2)
         c = 2.0 * atan2(sqrt(a), sqrt(1.0-a))
         return 6371 * 1000 * c
@@ -108,7 +132,7 @@ class ADSBModule(mp_module.MPModule):
                 #if not then add it
                 self.threat_vehicles[id] = ADSBVehicle(id = id, state = m.to_dict())
                 if self.mpstate.map: #if the map is loaded...
-                    icon = self.mpstate.map.icon(self.threat_vehicles[id].icon + '.png')
+                    icon = self.mpstate.map.icon(self.threat_vehicles[id].icon)
                     popup = MPMenuSubMenu('ADSB',
                                   items=[MPMenuItem(name=id, returnkey=None)])
                     #draw the vehicle on the map
@@ -121,50 +145,43 @@ class ADSBModule(mp_module.MPModule):
                     #update the map
                     self.mpstate.map.set_position(id, (m.lat*1e-7, m.lon*1e-7), rotation=m.heading)
                     
-        elif m.get_type() == "GLOBAL_POSITION_INT":
+        if m.get_type() == "GLOBAL_POSITION_INT":
             if self.mpstate.map:
+                if len(self.active_threats)>0:
+                    threat_circle_width = 2
+                else:
+                    threat_circle_width = 1
                 #update the threat circle on the map
                 threat_circle = mp_slipmap.SlipCircle("threat_circle", 3, (m.lat*1e-7, m.lon*1e-7), self.ADSB_settings.threat_radius,
-                                                              (0, 255, 255), linewidth=1, popup_menu=None)
+                                                              (0, 255, 255), linewidth=threat_circle_width)
                 threat_circle.set_hidden(not self.ADSB_settings.show_threat_radius) #show the circle?
                 self.mpstate.map.add_object(threat_circle)
                 
                 #update the threat clear circle on the map
                 threat_radius_clear = self.ADSB_settings.threat_radius*self.ADSB_settings.threat_radius_clear_multiplier
                 threat_clear_circle = mp_slipmap.SlipCircle("threat_clear_circle", 3, (m.lat*1e-7, m.lon*1e-7),threat_radius_clear,
-                                                              (0, 255, 255), linewidth=1, popup_menu=None)
+                                                              (0, 255, 255), linewidth=1)
                 threat_clear_circle.set_hidden(not self.ADSB_settings.show_threat_radius_clear) #show the circle?
                 self.mpstate.map.add_object(threat_clear_circle)
             
             #we assume this is handled much more oftern than ADS-B messages
             #so update the distance between vehicle and threat here
-            self.update_threat_distances((m.lat*1e-7, m.lon*1e-7, m.alt*1e-3))  
-        
-        else:
-            pass
+            self.update_threat_distances((m.lat*1e-7, m.lon*1e-7, m.alt*1e-3))
                     
     def idle_task(self):
         '''called on idle'''
         current_time = time.time()
-        threat_radius_clear = self.ADSB_settings.threat_radius*self.ADSB_settings.threat_radius_clear_multiplier
         
         for id in self.threat_vehicles.keys():
-            
-            if self.threat_vehicles[id].distance is not None:
-                if self.threat_vehicles[id].distance <= self.ADSB_settings.threat_radius and not self.threat_vehicles[id].is_evading_threat:
-                    #if the threat is in the threat radius and not currently known to the module...
-                    self.threat_vehicles[id].is_evading_threat = True #set flag to action threat
-                    
-                if self.threat_vehicles[id].distance > threat_radius_clear and self.threat_vehicles[id].is_evading_threat:
-                    #if the threat is known to the module and outside the threat clear radius...
-                    self.threat_vehicles[id].is_evading_threat = False #clear flag to action threat
-                    
             if current_time - self.threat_vehicles[id].update_time > self.ADSB_settings.timeout:
                 #if the treat has timed out...
                 del self.threat_vehicles[id] #remove the threat from the dict
                 if self.mpstate.map:
                     self.mpstate.map.remove_object(id) #remove the threat from the map
-                    
+        
+        if self.theat_detection_timer.trigger():
+            self.perform_threat_detection()
+            #TODO: possibly evade detected threats with ids in self.active_threats
                 
 def init(mpstate):
     '''initialise module'''
