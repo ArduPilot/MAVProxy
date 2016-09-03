13a4bdfa53fbd9f980efe4a1622a33c2c8c4c0f0
Stephen Dade <stephen_dade@hotmail.com>
2016-09-03 21:15:09 +1000

    Readkml: Added right-click menu to map and ability to load kml layer to geofence

--- MAVProxy/modules/mavproxy_kmlread.py
+++ MAVProxy/modules/mavproxy_kmlread.py
@@ -7,7 +7,7 @@ Released under the GNU GPL version 3 or later
 '''
 
 import time, math, random
-from pymavlink import mavutil
+from pymavlink import mavutil, mavwp
 from cStringIO import StringIO
 from xml.dom.minidom import parseString
 from zipfile import ZipFile
@@ -26,11 +26,28 @@ class KmlReadModule(mp_module.MPModule):
         self.add_command('kml', self.cmd_param, "kml map handling",
                          ["<clear>",
                           "<load> (FILENAME)", '<layers>'])
+                          
+        #allayers is all the loaded layers (slimap objects)
+        #curlayers is only the layers displayed (checked) on the map (text list of layer names)
+        self.allayers = []
         self.curlayers = []
+        self.alltextlayers = []
+        self.curtextlayers = []
+        self.menu_added_map = False
+        self.menu_needs_refreshing = True
+        
+        #the fence manager
+        self.fenceloader = mavwp.MAVFenceLoader()
+        
+        #make the initial map menu
+        if mp_util.has_wxpython:
+            self.menu_fence = MPMenuSubMenu('Set Geofence', items=[])
+            self.menu = MPMenuSubMenu('KML Layers',
+                                  items=[MPMenuItem('Clear', 'Clear', '# kml clear')])
                           
     def cmd_param(self, args):
         '''control kml reading'''
-        usage = "Usage: kml <clear | load (filename) | layers>"
+        usage = "Usage: kml <clear | load (filename) | layers | toggle (layername) | fence (layername)>"
         if len(args) < 1:
             print(usage)
             return
@@ -44,17 +61,123 @@ class KmlReadModule(mp_module.MPModule):
         elif args[0] == "layers":
             for layer in self.curlayers:
                 print "Found layer: " + layer
+        elif args[0] == "toggle":
+            self.togglekml(args[1])
+        elif args[0] == "fence":
+            self.fencekml(args[1])
         else:
             print(usage)
             return
 
+    def fencekml(self, layername):
+        '''set a layer as the geofence'''
+        #Strip quotation marks if neccessary
+        if layername.startswith('"') and layername.endswith('"'):
+            layername = layername[1:-1]
+        
+        #for each point in the layer, add it in
+        for layer in self.allayers:
+            if layer.key == layername:
+                #clear the current fence
+                self.fenceloader.clear()
+                if len(layer.points) < 3:
+                    return
+                self.fenceloader.target_system = self.target_system
+                self.fenceloader.target_component = self.target_component
+                #send centrepoint  to fence[0] as the return point
+                bounds = mp_util.polygon_bounds(layer.points)
+                (lat, lon, width, height) = bounds
+                center = (lat+width/2, lon+height/2)
+                self.fenceloader.add_latlon(center[0], center[1])
+                for lat, lon in layer.points:
+                    #add point
+                    self.fenceloader.add_latlon(lat, lon)
+                #and send
+                self.send_fence()
+
+    def send_fence(self):
+        '''send fence points from fenceloader. Taken from fence module'''
+        # must disable geo-fencing when loading
+        self.fenceloader.target_system = self.target_system
+        self.fenceloader.target_component = self.target_component
+        self.fenceloader.reindex()
+        action = self.get_mav_param('FENCE_ACTION', mavutil.mavlink.FENCE_ACTION_NONE)
+        self.param_set('FENCE_ACTION', mavutil.mavlink.FENCE_ACTION_NONE, 3)
+        self.param_set('FENCE_TOTAL', self.fenceloader.count(), 3)
+        for i in range(self.fenceloader.count()):
+            p = self.fenceloader.point(i)
+            self.master.mav.send(p)
+            p2 = self.fetch_fence_point(i)
+            if p2 is None:
+                self.param_set('FENCE_ACTION', action, 3)
+                return False
+            if (p.idx != p2.idx or
+                abs(p.lat - p2.lat) >= 0.00003 or
+                abs(p.lng - p2.lng) >= 0.00003):
+                print("Failed to send fence point %u" % i)
+                self.param_set('FENCE_ACTION', action, 3)
+                return False
+        self.param_set('FENCE_ACTION', action, 3)
+        return True
+
+    def fetch_fence_point(self ,i):
+        '''fetch one fence point. Taken from fence module'''
+        self.master.mav.fence_fetch_point_send(self.target_system,
+                                                    self.target_component, i)
+        tstart = time.time()
+        p = None
+        while time.time() - tstart < 3:
+            p = self.master.recv_match(type='FENCE_POINT', blocking=False)
+            if p is not None:
+                break
+            time.sleep(0.1)
+            continue
+        if p is None:
+            self.console.error("Failed to fetch point %u" % i)
+            return None
+        return p
+                                    
+    def togglekml(self, layername):
+        '''toggle the display of a kml'''
+        #Strip quotation marks if neccessary
+        if layername.startswith('"') and layername.endswith('"'):
+            layername = layername[1:-1]
+        #toggle layer off (plus associated text element)
+        if layername in self.curlayers:
+            for layer in self.curlayers:
+                if layer == layername:
+                    self.mpstate.map.remove_object(layer)
+                    self.curlayers.remove(layername)
+                    if layername in self.curtextlayers:
+                        for layer in self.curtextlayers:
+                            if layer == layername:
+                                self.mpstate.map.remove_object(layer)
+                                self.curtextlayers.remove(layer)
+        #toggle layer on (plus associated text element)
+        else:
+            for layer in self.allayers:
+                if layer.key == layername:
+                    self.mpstate.map.add_object(layer)
+                    self.curlayers.append(layername)
+                    for layer in self.alltextlayers:
+                        if layer.key == layername:
+                            self.mpstate.map.add_object(layer)
+                            self.curtextlayers.append(layername)
+        self.menu_needs_refreshing = True
+        
     def clearkml(self):
         '''Clear the kml's from the map'''
         #go through all the current layers and remove them
         for layer in self.curlayers:
             self.mpstate.map.remove_object(layer)
+        for layer in self.curtextlayers:
+            self.mpstate.map.remove_object(layer)
+        self.allayers = []
         self.curlayers = []
-        
+        self.alltextlayers = []
+        self.curtextlayers = []
+        self.menu_needs_refreshing = True
+                
     def loadkml(self, filename):
         '''Load a kml from file and put it on the map'''
         #Open the zip file
@@ -68,19 +191,53 @@ class KmlReadModule(mp_module.MPModule):
             if self.mpstate.map is not None and point[0] == 'Polygon':
                 #print "Adding " + point[1]
                 newcolour = (random.randint(0, 255), 0, random.randint(0, 255))
-                self.mpstate.map.add_object(mp_slipmap.SlipPolygon(point[1], point[2],
-                                                             layer=2, linewidth=2, colour=newcolour))
+                curpoly = mp_slipmap.SlipPolygon(point[1], point[2],
+                                                             layer=2, linewidth=2, colour=newcolour)
+                self.mpstate.map.add_object(curpoly)
+                self.allayers.append(curpoly)
                 self.curlayers.append(point[1])
                 
                 
-            #and points - 20m circle
+            #and points - barrell image and text
             if self.mpstate.map is not None and point[0] == 'Point':
                 #print "Adding " + point[1]
                 icon = self.mpstate.map.icon('barrell.png')
-                self.mpstate.map.add_object(mp_slipmap.SlipIcon(point[1], latlon = (point[2][0][0], point[2][0][1]), layer=3, img=icon, rotation=0, follow=False))
+                curpoint = mp_slipmap.SlipIcon(point[1], latlon = (point[2][0][0], point[2][0][1]), layer=3, img=icon, rotation=0, follow=False)
+                curtext = mp_slipmap.SlipLabel(point[1], point = (point[2][0][0], point[2][0][1]), layer=4, label=point[1], colour=(0,255,255))
+                self.mpstate.map.add_object(curpoint)
+                self.mpstate.map.add_object(curtext)
+                self.allayers.append(curpoint)
+                self.alltextlayers.append(curtext)
                 self.curlayers.append(point[1])
-                
+                self.curtextlayers.append(point[1])
+        self.menu_needs_refreshing = True
         
+
+    def idle_task(self):
+        '''handle GUI elements'''
+        if not self.menu_needs_refreshing:
+            return
+        if self.module('map') is not None and not self.menu_added_map:
+            self.menu_added_map = True
+            self.module('map').add_menu(self.menu)
+        #(re)create the menu
+        if mp_util.has_wxpython and self.menu_added_map:
+            # we don't dynamically update these yet due to a wx bug
+            self.menu.items = [ MPMenuItem('Clear', 'Clear', '# kml clear'), MPMenuItem('Load', 'Load', '# kml load ', handler=MPMenuCallFileDialog(flags=('open',), title='KML Load', wildcard='*.kml;*.kmz')), self.menu_fence, MPMenuSeparator() ]
+            self.menu_fence.items = []
+            for layer in self.allayers:
+                #if it's a polygon, add it to the "set Geofence" list
+                if isinstance(layer, mp_slipmap.SlipPolygon):
+                    self.menu_fence.items.append(MPMenuItem(layer.key, layer.key, '# kml fence \"' + layer.key + '\"'))
+                #then add all the layers to the menu, ensuring to check the active layers
+                #text elements aren't included on the menu
+                if layer.key in self.curlayers and layer.key[-5:] != "-text":
+                    self.menu.items.append(MPMenuCheckbox(layer.key, layer.key, '# kml toggle \"' + layer.key + '\"', checked=True))
+                elif layer.key[-5:] != "-text":
+                    self.menu.items.append(MPMenuCheckbox(layer.key, layer.key, '# kml toggle \"' + layer.key + '\"', checked=False))
+            #and add the menu to the map popu menu
+            self.module('map').add_menu(self.menu)
+                            
     def mavlink_packet(self, m):
         '''handle a mavlink packet'''
            
