a4cc9c2ca2772ba46acfc18691c26e8f94fa7e2a
Dr.-Ing. Amilcar Do Carmo Lucas <amilcar.lucas@iav.de>
2017-01-12 17:33:49 +0100

    Add a more flexible innosetup configuration

--- windows/MAVProxyWinBuild.bat
+++ windows/MAVProxyWinBuild.bat
@@ -1,11 +1,14 @@
 rem build the standalone MAVProxy.exe for Windows.
 rem This assumes Python is installed in C:\Python27
+rem   If it is not, change the PYTHON_LOCATION environment variable accordingly
+rem This assumes InnoSetup is installed in C:\Program Files (x86)\Inno Setup 5
+rem   If it is not, change the INNOSETUP environment variable accordingly
 rem This requires Pyinstaller==2.1, setuptools==19.2 and packaging==14.2
 rem and lxml >= 3.6.4
 SETLOCAL enableextensions
 
-rem change this path if Python is installed somewhere non-standard
-set PYTHON_LOCATION=C:\Python27
+if "%PYTHON_LOCATION%" == "" (set PYTHON_LOCATION=C:\Python27)
+if "%INNOSETUP%" == "" (set INNOSETUP="C:\Program Files (x86)\Inno Setup 5")
 
 rem get the version
 for /f "tokens=*" %%a in (
@@ -33,6 +36,7 @@ rem -----Create version Info-----
 
 rem -----Build the Installer-----
 cd  ..\windows\
-"C:\Program Files (x86)\Inno Setup 5\ISCC.exe" /dMyAppVersion=%VERSION% mavproxy.iss
+rem Newer Inno Setup versions do not require a -compile flag, please add it if you have an old version
+%INNOSETUP%\ISCC.exe /dMyAppVersion=%VERSION% mavproxy.iss
 
 pause
