284812f95b50f432daddd6452afe07054d881eae
Dr.-Ing. Amilcar Do Carmo Lucas <amilcar.lucas@iav.de>
2017-01-12 18:24:36 +0100

    Correct handling of whitespaces in paths

--- windows/MAVProxyWinBuild.bat
+++ windows/MAVProxyWinBuild.bat
@@ -7,8 +7,8 @@ rem This requires Pyinstaller==2.1, setuptools==19.2 and packaging==14.2
 rem and lxml >= 3.6.4
 SETLOCAL enableextensions
 
-if "%PYTHON_LOCATION%" == "" (set PYTHON_LOCATION=C:\Python27)
-if "%INNOSETUP%" == "" (set INNOSETUP="C:\Program Files (x86)\Inno Setup 5")
+if "%PYTHON_LOCATION%" == "" ("set PYTHON_LOCATION=C:\Python27")
+if "%INNOSETUP%" == "" ("set INNOSETUP=C:\Program Files (x86)\Inno Setup 5")
 
 rem get the version
 for /f "tokens=*" %%a in (
@@ -18,16 +18,16 @@ for /f "tokens=*" %%a in (
  )
 
 rem -----build the changelog-----
-%PYTHON_LOCATION%\python createChangelog.py
+"%PYTHON_LOCATION%\python" createChangelog.py
 
 rem -----Upgrade pymavlink if needed-----
-%PYTHON_LOCATION%\Scripts\pip install pymavlink -U
+"%PYTHON_LOCATION%\Scripts\pip" install pymavlink -U
 
 rem -----Build MAVProxy-----
 cd ..\
-%PYTHON_LOCATION%\python setup.py clean build install
+"%PYTHON_LOCATION%\python" setup.py clean build install
 cd .\MAVProxy
-%PYTHON_LOCATION%\Scripts\pyinstaller --clean ..\windows\mavproxy.spec
+"%PYTHON_LOCATION%\Scripts\pyinstaller" --clean ..\windows\mavproxy.spec
 
 rem -----Create version Info-----
 @echo off
@@ -37,6 +37,6 @@ rem -----Create version Info-----
 rem -----Build the Installer-----
 cd  ..\windows\
 rem Newer Inno Setup versions do not require a -compile flag, please add it if you have an old version
-%INNOSETUP%\ISCC.exe /dMyAppVersion=%VERSION% mavproxy.iss
+"%INNOSETUP%\ISCC.exe" /dMyAppVersion=%VERSION% mavproxy.iss
 
 pause
