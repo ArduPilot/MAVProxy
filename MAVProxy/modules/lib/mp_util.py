951034f00b3d4c540343efbf90d01e57ea3bf593
samuel dudley <dudley.samuel@gmail.com>
2017-06-16 02:08:49 +0930

    add debug prints to try and capture map error detail on windows

--- MAVProxy/modules/lib/mp_util.py
+++ MAVProxy/modules/lib/mp_util.py
@@ -149,6 +149,8 @@ class object_container:
     containing all of the normal attributes of the original'''
     def __init__(self, object):
         for v in dir(object):
+            print(type(object))
+            print(dir(object))
             if not v.startswith('__') and v not in ['this' ,'m_altDown', 'm_controlDown', 'm_metaDown', 'm_shiftDown']:
                 try:
                     a = getattr(object, v)
