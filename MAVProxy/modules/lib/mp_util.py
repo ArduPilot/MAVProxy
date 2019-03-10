11f6fc2d29160fd595e19dc2c7d1cbc18bf5c8bf
Andrew Tridgell <andrew@tridgell.net>
2019-03-11 10:59:10 +1100

    util: added new devid types

--- MAVProxy/modules/lib/mp_util.py
+++ MAVProxy/modules/lib/mp_util.py
@@ -359,6 +359,7 @@ def decode_devid(devid, pname):
         0x0E : "DEVTYPE_MAG3110",
         0x0F : "DEVTYPE_SITL",
         0x10 : "DEVTYPE_IST8308",
+        0x11 : "DEVTYPE_RM3100",
         }
 
     imu_types = {
@@ -378,6 +379,11 @@ def decode_devid(devid, pname):
         0x28 : "DEVTYPE_INS_ICM20689",
         0x29 : "DEVTYPE_INS_BMI055",
         0x2A : "DEVTYPE_SITL",
+        0x2B : "DEVTYPE_INS_BMI088",
+        0x2C : "DEVTYPE_INS_ICM20948",
+        0x2D : "DEVTYPE_INS_ICM20648",
+        0x2E : "DEVTYPE_INS_ICM20649",
+        0x2F : "DEVTYPE_INS_ICM20602",
         }
 
     decoded_devname = ""
