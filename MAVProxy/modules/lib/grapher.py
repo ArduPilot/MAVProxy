5b4f2a3a99e1ac99b6fbcff368abd3925a9599fa
Andrew Tridgell <andrew@tridgell.net>
2019-05-19 18:33:10 +1000

    lib: allow grapher.py to generate to file

--- MAVProxy/modules/lib/grapher.py
+++ MAVProxy/modules/lib/grapher.py
@@ -189,9 +189,10 @@ class MavGraph(object):
         '''called on draw events'''
         self.draw_events += 1
 
-    def plotit(self, x, y, fields, colors=[], title=None):
+    def plotit(self, x, y, fields, colors=[], title=None, interactive=True):
         '''plot a set of graphs using date for x axis'''
-        pylab.ion()
+        if interactive:
+            pylab.ion()
         self.fig = pylab.figure(num=1, figsize=(12,6))
         self.ax1 = self.fig.gca()
         ax2 = None
@@ -479,7 +480,7 @@ class MavGraph(object):
             self.process_mav(mlog, flightmode_selections)
 
 
-    def show(self, lenmavlist, block=True, xlim_pipe=None):
+    def show(self, lenmavlist, block=True, xlim_pipe=None, output=None):
         '''show graph'''
         if xlim_pipe is not None:
             xlim_pipe[0].close()
@@ -507,20 +508,32 @@ class MavGraph(object):
                 col = colors[:]
             else:
                 col = colors[fi*len(self.fields):]
-            self.plotit(self.x, self.y, lab, colors=col, title=self.title)
+            interactive = True
+            if output is not None:
+                interactive = False
+            self.plotit(self.x, self.y, lab, colors=col, title=self.title, interactive=interactive)
             for i in range(0, len(self.x)):
                 self.x[i] = []
                 self.y[i] = []
 
-        if self.xlim_pipe is not None:
+        if self.xlim_pipe is not None and output is None:
             import matplotlib.animation
             self.ani = matplotlib.animation.FuncAnimation(self.fig, self.xlim_change_check,
                                                           frames=10, interval=20000,
                                                           repeat=True, blit=False)
             threading.Timer(0.1, self.xlim_timer).start()
 
-        pylab.draw()
-        pylab.show(block=block)
+        if output is None:
+            pylab.draw()
+            pylab.show(block=block)
+        elif output.endswith(".html"):
+            import mpld3
+            html = mpld3.fig_to_html(self.fig)
+            f_out = open(output, 'w')
+            f_out.write(html)
+            f_out.close()
+        else:
+            pylab.savefig(output, bbox_inches='tight', dpi=200)
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
@@ -535,6 +548,7 @@ if __name__ == "__main__":
     parser.add_argument("--marker", default=None, help="point marker")
     parser.add_argument("--linestyle", default=None, help="line style")
     parser.add_argument("--xaxis", default=None, help="X axis expression")
+    parser.add_argument("--title", default=None, help="set title")
     parser.add_argument("--multi", action='store_true', help="multiple files with same colours")
     parser.add_argument("--zero-time-base", action='store_true', help="use Z time base for DF logs")
     parser.add_argument("--show-flightmode", default=True,
@@ -562,6 +576,7 @@ if __name__ == "__main__":
     mg.set_legend(args.legend)
     mg.set_legend2(args.legend2)
     mg.set_multi(args.multi)
+    mg.set_title(args.title)
     mg.set_show_flightmode(args.show_flightmode)
-    mg.process([],0)
-    mg.show(len(mg.mav_list))
+    mg.process([],[],0)
+    mg.show(len(mg.mav_list), output=args.output)
