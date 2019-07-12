aceb45e9ddc45cd72aef1a35ab572ff5666a2f6b
Andrew Tridgell <andrew@tridgell.net>
2019-07-12 14:41:00 +1000

    remove OTG2 dual-CDC devices in auto serial list

--- MAVProxy/mavproxy.py
+++ MAVProxy/mavproxy.py
@@ -1136,6 +1136,13 @@ if __name__ == '__main__':
         load_module('speech')
 
     serial_list = mavutil.auto_detect_serial(preferred_list=preferred_ports)
+    serial_list.sort(key=lambda x: x.device)
+
+    # remove OTG2 ports for dual CDC
+    if len(serial_list) == 2 and serial_list[0].device.startswith("/dev/serial/by-id"):
+        if serial_list[0].device[:-1] == serial_list[1].device[0:-1]:
+            serial_list.pop(1)
+
     if not opts.master:
         print('Auto-detected serial ports are:')
         for port in serial_list:
