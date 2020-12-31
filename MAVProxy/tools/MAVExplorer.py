c3268d52ff9a92ee60e60351aff4d16f436d859a
Andrew Tridgell <andrew@tridgell.net>
2021-01-01 06:10:18 +1100

    MAVExplorer: fixed devid for new baro params

--- MAVProxy/tools/MAVExplorer.py
+++ MAVProxy/tools/MAVExplorer.py
@@ -793,6 +793,8 @@ def cmd_devid(args):
             mp_util.decode_devid(params[p], p)
         if p.startswith('GND_BARO') and p.endswith('_ID'):
             mp_util.decode_devid(params[p], p)
+        if p.startswith('BARO') and p.endswith('DEVID'):
+            mp_util.decode_devid(params[p], p)
 
 def cmd_loadfile(args):
     '''callback from menu to load a log file'''
