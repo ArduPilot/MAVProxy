da1594e5409c65e4f0874d746b6b2dfcb6b0612b
Rhys Mainwaring <rhys.mainwaring@me.com>
2021-03-02 14:32:21 +0000

    live_graph: fix for #867 on macOS
    Update backend_wxagg.py for macOS to account for interface changes in the matplotlib.backends module.
    These changes tested on Python 3.9.1 and matplotlib 3.3.3
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- MAVProxy/modules/lib/MacOS/backend_wxagg.py
+++ MAVProxy/modules/lib/MacOS/backend_wxagg.py
@@ -2,13 +2,13 @@ from __future__ import division, print_function
 import matplotlib
 from matplotlib.figure import Figure
 
-from backend_agg import FigureCanvasAgg
-import backend_wx    # already uses wxversion.ensureMinimal('2.8')
-from backend_wx import FigureManager, FigureManagerWx, FigureCanvasWx, \
-    FigureFrameWx, DEBUG_MSG, NavigationToolbar2Wx, error_msg_wx, \
-    draw_if_interactive, show, Toolbar, backend_version
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.backends import backend_wx    # already uses wxversion.ensureMinimal('2.8')
+from matplotlib.backends.backend_wx import DEBUG_MSG, \
+    error_msg_wx, NavigationToolbar2Wx
+from matplotlib.backends.backend_wxagg import FigureManager, FigureCanvasWxAgg, \
+    FigureFrameWx, draw_if_interactive, show, backend_version
 import wx
-
 
 class FigureFrameWxAgg(FigureFrameWx):
     def get_canvas(self, fig):
@@ -16,80 +16,13 @@ class FigureFrameWxAgg(FigureFrameWx):
 
     def _get_toolbar(self, statbar):
         if matplotlib.rcParams['toolbar']=='classic':
-            toolbar = NavigationToolbarWx(self.canvas, True)
+            toolbar = NavigationToolbar2Wx(self.canvas, True)
         elif matplotlib.rcParams['toolbar']=='toolbar2':
             toolbar = NavigationToolbar2WxAgg(self.canvas)
             toolbar.set_status_bar(statbar)
         else:
             toolbar = None
         return toolbar
-
-
-class FigureCanvasWxAgg(FigureCanvasAgg, FigureCanvasWx):
-    """
-    The FigureCanvas contains the figure and does event handling.
-
-    In the wxPython backend, it is derived from wxPanel, and (usually)
-    lives inside a frame instantiated by a FigureManagerWx. The parent
-    window probably implements a wxSizer to control the displayed
-    control size - but we give a hint as to our preferred minimum
-    size.
-    """
-
-    def draw(self, drawDC=None):
-        """
-        Render the figure using agg.
-        """
-        DEBUG_MSG("draw()", 1, self)
-        FigureCanvasAgg.draw(self)
-
-        self.bitmap = _convert_agg_to_wx_bitmap(self.get_renderer(), None)
-        self._isDrawn = True
-        self.gui_repaint(drawDC=drawDC)
-
-    def blit(self, bbox=None):
-        """
-        Transfer the region of the agg buffer defined by bbox to the display.
-        If bbox is None, the entire buffer is transferred.
-        """
-        if bbox is None:
-            self.bitmap = _convert_agg_to_wx_bitmap(self.get_renderer(), None)
-            self.gui_repaint()
-            return
-
-        l, b, w, h = bbox.bounds
-        r = l + w
-        t = b + h
-        x = int(l)
-        y = int(self.bitmap.GetHeight() - t)
-
-        srcBmp = _convert_agg_to_wx_bitmap(self.get_renderer(), None)
-        srcDC = wx.MemoryDC()
-        srcDC.SelectObject(srcBmp)
-
-        destDC = wx.MemoryDC()
-        destDC.SelectObject(self.bitmap)
-
-        destDC.BeginDrawing()
-        destDC.Blit(x, y, int(w), int(h), srcDC, x, y)
-        destDC.EndDrawing()
-
-        destDC.SelectObject(wx.NullBitmap)
-        srcDC.SelectObject(wx.NullBitmap)
-        self.gui_repaint()
-
-    filetypes = FigureCanvasAgg.filetypes
-
-    def print_figure(self, filename, *args, **kwargs):
-        # Use pure Agg renderer to draw
-        FigureCanvasAgg.print_figure(self, filename, *args, **kwargs)
-        # Restore the current view; this is needed because the
-        # artist contains methods rely on particular attributes
-        # of the rendered figure for determining things like
-        # bounding boxes.
-        if self._isDrawn:
-            self.draw()
-
 
 class NavigationToolbar2WxAgg(NavigationToolbar2Wx):
     def get_canvas(self, frame, fig):
