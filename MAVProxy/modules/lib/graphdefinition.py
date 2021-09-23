04a7eaf3167bab06d34314ebffd5c4d13acf5f97
Matt Kear <matthewnkear@gmail.com>
2021-09-23 21:14:24 +0100

    GraphDefinition:  Add label override var to class

--- MAVProxy/modules/lib/graphdefinition.py
+++ MAVProxy/modules/lib/graphdefinition.py
@@ -4,9 +4,10 @@ GraphDefinition class
 
 class GraphDefinition(object):
     '''a pre-defined graph'''
-    def __init__(self, name, expression, description, expressions, filename):
+    def __init__(self, name, expression, description, expressions, filename, label_or=None):
         self.name = name
         self.expression = expression
         self.description = description
         self.expressions = expressions
         self.filename = filename
+        self.label_override = label_or
