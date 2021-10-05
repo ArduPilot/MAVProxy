67be1e88d2efa7a6daa941f91c80f0014f645082
crashmatt <uavflightdirector@gmail.com>
2021-10-05 14:18:02 +0200

    messagerate: set and get message stream rates

--- MAVProxy/modules/mavproxy_messagerate.py
+++ MAVProxy/modules/mavproxy_messagerate.py
@@ -9,7 +9,7 @@ Simply display message rates
 import time
 
 from MAVProxy.modules.lib import mp_module
-
+from pymavlink import mavutil
 
 class messagerate(mp_module.MPModule):
     def __init__(self, mpstate):
@@ -23,11 +23,11 @@ class messagerate(mp_module.MPModule):
         self.add_command('messagerate',
                          self.cmd_messagerate,
                          "messagerate module",
-                         ['status', 'reset'])
+                         ['status', 'reset', 'set', 'get'])
 
     def usage(self):
         '''show help on command line options'''
-        return "Usage: messagerate <status>"
+        return "Usage: messagerate <status | reset | set(msg)(rate) | get(msg)>"
 
     def cmd_messagerate(self, args):
         '''control behaviour of the module'''
@@ -37,6 +37,45 @@ class messagerate(mp_module.MPModule):
             print(self.status())
         elif args[0] == "reset":
             self.reset()
+        elif args[0] == "get":
+            if len(args) != 2:
+                print(self.usage())
+                return
+            message_name = args[1]
+            mavlink_map = mavutil.mavlink.mavlink_map
+            for msg_id in mavlink_map.keys():
+                mav_cmd_name = mavlink_map[msg_id].name
+                if mav_cmd_name == message_name:
+                    self.master.mav.command_long_send(
+                        self.settings.target_system,
+                        self.settings.target_component,
+                        mavutil.mavlink.MAV_CMD_GET_MESSAGE_INTERVAL,
+                        0,
+                        msg_id, 0, 0, 0, 0, 0, 0)
+                    return
+            print("Unknown message ID:%s" % message_name)
+        elif args[0] == "set":
+            if len(args) < 3:
+                print(self.usage())
+                return
+            message_name = args[1]
+            message_rate = float(args[2])
+            priority = 0
+            if len(args) > 3:
+              priority = int(args[3])
+            mavlink_map = mavutil.mavlink.mavlink_map
+            for msg_id in mavlink_map.keys():
+                mav_cmd_name = mavlink_map[msg_id].name
+                if mav_cmd_name == message_name:
+                    self.master.mav.command_long_send(
+                        self.settings.target_system,
+                        self.settings.target_component,
+                        mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,
+                        0,
+                        msg_id, (int) (1E6/message_rate), priority, 0, 0, 0, 0)
+                    return
+            print("Unknown message ID:%s" % message_name)
+            
         else:
             print(self.usage())
 
@@ -80,6 +119,14 @@ class messagerate(mp_module.MPModule):
         if mtype not in self.counts:
             self.counts[mtype] = 0
         self.counts[mtype] += 1
+        
+        mavlink_map = mavutil.mavlink.mavlink_map
+              
+        if mtype ==  'MESSAGE_INTERVAL':
+            if  m.message_id in mavlink_map:
+                print("Msg:%s  rate:%0.2fHz" % (mavlink_map[m.message_id].name, (1E6/m.interval_us) ) )
+            else:
+                print("Msg ID:%s  rate:%0.2fHz" % (m.message_id, (1E6/m.interval_us) ) )
 
 
 def init(mpstate):
