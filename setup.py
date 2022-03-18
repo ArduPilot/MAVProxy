6e1d26ed0a4dbe322b9ec526b919f9fc51e093c7
Andrew Tridgell <andrew@tridgell.net>
2022-03-18 11:38:18 +1100

    revert the pymavlink dependency change
    this breaks CI on older ArduPilot branches

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ package_data.extend(package_files('MAVProxy/modules/mavproxy_cesium/app'))
 # as that breaks the pip install. It seems that pip is not smart enough to
 # use the system versions of these dependencies, so it tries to download and install
 # large numbers of modules like numpy etc which may be already installed
-requirements=['pymavlink>=2.4.29',
+requirements=['pymavlink>=2.4.14',
               'pyserial>=3.0']
 
 if platform.system() == "Darwin":
