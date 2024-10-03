9537dc2a0f444bf05947dc4ac8feac5635f08bc8
Peter Barker <pbarker@barker.dropbear.id.au>
2024-10-03 19:28:23 +1000

    mavproxy_help.py: flake8 compliance

--- MAVProxy/modules/mavproxy_help.py
+++ MAVProxy/modules/mavproxy_help.py
@@ -1,39 +1,42 @@
 """
     MAVProxy help/versioning module
+
+AP_FLAKE8_CLEAN
 """
-import os, time, platform, re, sys, socket
-from pymavlink import mavwp, mavutil
+import platform
+import re
+import socket
+import sys
+
 from MAVProxy.modules.lib import mp_util
 from MAVProxy.modules.lib import mp_module
 if mp_util.has_wxpython:
     import wx
-    from MAVProxy.modules.lib.mp_menu import *
+    from MAVProxy.modules.lib.mp_menu import MPMenuItem
+    from MAVProxy.modules.lib.mp_menu import MPMenuSubMenu
+    from MAVProxy.modules.lib.mp_menu import MPMenuOpenWeblink
+    from MAVProxy.modules.lib.mp_menu import MPMenuChildMessageDialog
 
 if sys.version_info.major < 3:
-    from urllib2 import Request
-    from urllib2 import urlopen
-    from urllib2 import URLError, HTTPError
     import xmlrpclib
 else:
-    from urllib.request import Request
-    from urllib.request import urlopen
-    from urllib.error import URLError, HTTPError
     basestring = str
     import xmlrpc.client as xmlrpclib
- 
+
+
 class HelpModule(mp_module.MPModule):
     def __init__(self, mpstate):
-        super(HelpModule, self).__init__(mpstate, "mavhelp", "Help and version information", public = True)
+        super(HelpModule, self).__init__(mpstate, "mavhelp", "Help and version information", public = True)  # noqa
         self.enabled = False
         self.add_command('mavhelp', self.cmd_help, "help and version information", "<about|site>")
         self.have_list = False
 
-        #versioning info
-        #pkg_resources doesn't work in the windows exe build, so read the version file
+        #  versioning info
+        #  pkg_resources doesn't work in the windows exe build, so read the version file
         try:
             import pkg_resources
             self.version = pkg_resources.Environment()["mavproxy"][0].version
-        except:
+        except Exception:
             start_script = mp_util.dot_mavproxy("version.txt")
             f = open(start_script, 'r')
             self.version = f.readline()
@@ -44,20 +47,20 @@ class HelpModule(mp_module.MPModule):
         else:
             self.wxVersion = ''
 
-        #check for updates, if able
+        #  check for updates, if able
         pypi = xmlrpclib.ServerProxy('https://pypi.python.org/pypi')
         available = None
         try:
             available = pypi.package_releases('MAVProxy')
         except socket.gaierror:
             pass
-            
+
         if available:
             self.newversion = available[0]
         else:
             self.newversion = 'Error finding update'
-            
-        #and format the update string
+
+        #  and format the update string
         if not isinstance(self.newversion, basestring):
             self.newversion = "Error finding update"
         elif re.search('[a-zA-Z]', self.newversion):
@@ -69,19 +72,36 @@ class HelpModule(mp_module.MPModule):
 
         if mp_util.has_wxpython:
             self.menu_added_console = False
-            self.menu = MPMenuSubMenu('Help',
-                                  items=[MPMenuItem('MAVProxy website', 'MAVProxy website', '', handler=MPMenuOpenWeblink('https://ardupilot.org/mavproxy/index.html')),
-                                         MPMenuItem('Check for Updates', 'Check for Updates', '', handler=MPMenuChildMessageDialog(title="Updates", message=self.newversion)),
-                                         MPMenuItem('About', 'About', '', handler=MPMenuChildMessageDialog(title="About MAVProxy", message=self.about_string()))])
+            self.menu = MPMenuSubMenu(
+                'Help',
+                items=[
+                    MPMenuItem(
+                        'MAVProxy website',
+                        'MAVProxy website',
+                        '',
+                        handler=MPMenuOpenWeblink('https://ardupilot.org/mavproxy/index.html'),
+                    ),
+                    MPMenuItem(
+                        'Check for Updates',
+                        'Check for Updates',
+                        '',
+                        handler=MPMenuChildMessageDialog(
+                            title="Updates",
+                            message=self.newversion,
+                        )
+                    ),
+                    MPMenuItem(
+                        'About',
+                        'About',
+                        '',
+                        handler=MPMenuChildMessageDialog(
+                            title="About MAVProxy",
+                            message=self.about_string(),
+                        ))
+                ])
 
     def about_string(self):
-        return "MAVProxy Version " + self.version + "\nOS: " + self.host + "\nPython " +  self.pythonversion + "\nWXPython " + self.wxVersion
-
-    #version number comparison for update checking
-    def mycmp(self, version1, version2):
-        def normalize(v):
-            return [int(x) for x in re.sub(r'(\.0+)*$','', v).split(".")]
-        return cmp(normalize(version1), normalize(version2))
+        return "MAVProxy Version " + self.version + "\nOS: " + self.host + "\nPython " + self.pythonversion + "\nWXPython " + self.wxVersion  # noqa
 
     def idle_task(self):
         '''called on idle'''
@@ -110,6 +130,7 @@ class HelpModule(mp_module.MPModule):
 
     def print_usage(self):
         print("usage: mavhelp <about|site>")
+
 
 def init(mpstate):
     '''initialise module'''
