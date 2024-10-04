272eb79b071816302cda8aa7f599aa0d0e6d53f3
Peter Barker <pbarker@barker.dropbear.id.au>
2024-10-04 10:56:42 +1000

    windows: include requests in package
    so mavproxy_help works

--- windows/MAVProxyWinBuild.bat
+++ windows/MAVProxyWinBuild.bat
@@ -35,6 +35,7 @@ python.exe -m pip install -U wheel setuptools pip
 python.exe -m pip install pywin32 lxml pymavlink numpy matplotlib pyserial opencv-python PyYAML Pygame Pillow wxpython prompt-toolkit scipy
 python.exe -m pip install -U openai wave pyaudio
 python.exe -m pip install -U pyinstaller==6.7.0 packaging 
+python.exe -m pip install -U requests
 
 rem -----Build MAVProxy-----
 cd ..\
