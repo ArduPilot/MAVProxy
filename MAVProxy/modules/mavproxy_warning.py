7f49ac9582db96930c0a384bbcc4cdfcbf152715
Michelle Rossouw <michelleros128@gmail.com>
2025-03-26 00:41:38 +1100

    Warning: Support for message being missing (usually indicating that sensor is gone), check terrain and change text for details to grey.

--- MAVProxy/modules/mavproxy_warning.py
+++ MAVProxy/modules/mavproxy_warning.py
@@ -23,8 +23,9 @@ class WarningModule(mp_module.MPModule):
               ('min_sat', int, 9),
               ('sat_diff', int, 5),
               ('rpm_diff', int, 1000),
-              ('airspd_diff', int, 5),
-              ('max_wind', int, 10),
+              ('airspd_diff', float, 5),
+              ('max_wind', float, 10),
+              ('max_agl_alt', float, 123),
           ])
 
 
@@ -44,66 +45,92 @@ class WarningModule(mp_module.MPModule):
         fwd_escs = [ 0, 2 ]
         vtol_escs = [ 1, 3 ]
 
-        #  GPS_RAW_INT = self.master.messages.get("GPS_RAW_INT", None)
-        gps1 = self.master.messages["GPS_RAW_INT"]
-        gps2 = self.master.messages["GPS2_RAW"]
-        esc = self.master.messages["ESC_TELEMETRY_1_TO_4"]
-        vfr_hud = self.master.messages["VFR_HUD"]
-        nvf_as2 = self.master.messages["NAMED_VALUE_FLOAT"]['AS2']
+        gps1 = self.master.messages.get("GPS_RAW_INT", None)
+        gps2 = self.master.messages.get("GPS2_RAW", None)
+        esc = self.master.messages.get("ESC_TELEMETRY_1_TO_4", None)
+        vfr_hud = self.master.messages.get("VFR_HUD", None)
+        terr = self.master.messages.get("TERRAIN_REPORT", None)
+        nvf_as2 = self.master.messages.get("NAMED_VALUE_FLOAT", None)
+        if nvf_as2 != None:
+            nvf_as2 = nvf_as2['AS2']
 
-        #check min visible sat
-        if (gps1.satellites_visible < self.warning_settings.min_sat):
-            self.details = "GPS1 satellites less than " + str(self.warning_settings.min_sat)
-            self.warn("GPS") 
-
-        if (gps2.satellites_visible < self.warning_settings.min_sat):
-            self.details = "GPS2 satellites less than " + str(self.warning_settings.min_sat)
-            self.warn("GPS") 
-
-        #check difference in sat counts
-        if abs(gps1.satellites_visible - gps2.satellites_visible) > self.warning_settings.sat_diff: 
-            self.details = "Satellite diff between GPS1 and GPS2 more than " + str(self.warning_settings.sat_diff)
+        if gps1 is None or gps2 is None:
+            self.details = "No GPS1 and/or GPS2 message"
             self.warn("GPS")
+        else:
+            #check min visible sat
+            if gps1.satellites_visible < self.warning_settings.min_sat:
+                self.details = "GPS1 satellites less than " + str(self.warning_settings.min_sat)
+                self.warn("GPS") 
+
+            if gps2.satellites_visible < self.warning_settings.min_sat:
+                self.details = "GPS2 satellites less than " + str(self.warning_settings.min_sat)
+                self.warn("GPS") 
+
+            #check difference in sat counts
+            if abs(gps1.satellites_visible - gps2.satellites_visible) > self.warning_settings.sat_diff: 
+                self.details = "Satellite diff between GPS1 and GPS2 more than " + str(self.warning_settings.sat_diff)
+                self.warn("GPS")
 
         #check fwd esc rpms
-        max_esc = 0
-        min_esc = 1e9
-        for no in fwd_escs:
-            # print("ESC number: " + str(no))
-            if esc.rpm[no] > max_esc:
-                max_esc = esc.rpm[no]
-            if esc.rpm[no] < min_esc:
-                min_esc = esc.rpm[no]
-
-        if (max_esc - min_esc) > self.warning_settings.rpm_diff:
-            self.details = "FWD ESC difference between max and min RPM more than " + str(self.warning_settings.rpm_diff)
+        if esc is None:
+            self.details = "No ESC message"
             self.warn("ESC")
+        else:
+            max_esc = 0
+            min_esc = 1e9
+            for no in fwd_escs:
+                # print("ESC number: " + str(no))
+                if esc.rpm[no] > max_esc:
+                    max_esc = esc.rpm[no]
+                if esc.rpm[no] < min_esc:
+                    min_esc = esc.rpm[no]
 
-        #check vtol esc rpms
-        max_esc = 0
-        min_esc = 1e9
-        for no in vtol_escs:
-            # print("ESC number: " + str(no))
-            if esc.rpm[no] > max_esc:
-                max_esc = esc.rpm[no]
-            if esc.rpm[no] < min_esc:
-                min_esc = esc.rpm[no]
+            if (max_esc - min_esc) > self.warning_settings.rpm_diff:
+                self.details = "FWD ESC difference between max and min RPM more than " + str(self.warning_settings.rpm_diff)
+                self.warn("ESC")
 
-        if (max_esc - min_esc) > self.warning_settings.rpm_diff:
-            self.details = "VTOL ESC difference between max and min RPM more than " + str(self.warning_settings.rpm_diff)
-            self.warn("ESC")
+            #check vtol esc rpms
+            max_esc = 0
+            min_esc = 1e9
+            for no in vtol_escs:
+                # print("ESC number: " + str(no))
+                if esc.rpm[no] > max_esc:
+                    max_esc = esc.rpm[no]
+                if esc.rpm[no] < min_esc:
+                    min_esc = esc.rpm[no]
+
+            if (max_esc - min_esc) > self.warning_settings.rpm_diff:
+                self.details = "VTOL ESC difference between max and min RPM more than " + str(self.warning_settings.rpm_diff)
+                self.warn("ESC")
 
         if not (self.sensor_enabled['AS'] and self.sensor_present['AS'] and self.sensor_healthy['AS']):
             self.details = "Airspeed sensor issue: Enabled: " + str(self.sensor_enabled['AS']) + ", Present: " + str(self.sensor_present['AS']) + ", Healthy: " + str(self.sensor_healthy['AS'])
             self.warn("Airspeed")
 
-        if abs(vfr_hud.airspeed - nvf_as2.value) > self.warning_settings.airspd_diff:
-            self.details = "Airspeed 1 vs airspeed 2 difference greater than " + str(self.warning_settings.airspd_diff)
+        if vfr_hud is None or nvf_as2 is None:
+            self.details = "No HUD and/or AS2 message"
             self.warn("Airspeed")
+        else:
+            if abs(vfr_hud.airspeed - nvf_as2.value) > self.warning_settings.airspd_diff:
+                self.details = "Airspeed 1 vs airspeed 2 difference greater than " + str(self.warning_settings.airspd_diff)
+                self.warn("Airspeed")
 
-        if abs(vfr_hud.groundspeed - (vfr_hud.airspeed + nvf_as2.value)/2) > self.warning_settings.max_wind:
-            self.details = "Airspeed vs groundspeed difference more than " + str(self.warning_settings.airspd_diff)
-            self.warn("Airspeed")
+            if abs(vfr_hud.groundspeed - (vfr_hud.airspeed + nvf_as2.value)/2) > self.warning_settings.max_wind:
+                self.details = "Airspeed vs groundspeed difference more than " + str(self.warning_settings.max_wind)
+                self.warn("Airspeed")
+            
+        if terr is None:
+            self.details = "No terrain message."
+            self.warn("Terrain")
+        else:
+            if terr.pending > 0:
+                self.details = "Pending terrain items."
+                self.warn("Terrain")
+
+            if terr.current_height > self.warning_settings.max_agl_alt:
+                self.details = "AGL Alt more than " + str(self.warning_settings.max_agl_alt)
+                self.warn("Terrain")
 
 
     def update_status(self):
@@ -130,7 +157,7 @@ class WarningModule(mp_module.MPModule):
     def warn(self, err):
         self.say("Warning " + err + " failure")
         if self.warning_settings.details == True and self.details != "":
-            self.console.writeln(self.details)
+            self.console.writeln(self.details, fg = 'grey')
             self.details = ""
 
 
