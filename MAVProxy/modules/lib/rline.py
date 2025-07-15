c13a5b66f6220b7bafd5c52149b4ace88e32f1c9
Christian Clauss <cclauss@me.com>
2025-07-15 12:03:05 +0200

    Update MAVProxy/modules/lib/rline.py
    Co-authored-by: Peter Barker <pb-gh@barker.dropbear.id.au>

--- MAVProxy/modules/lib/rline.py
+++ MAVProxy/modules/lib/rline.py
@@ -74,7 +74,7 @@ class rline(object):
         '''redisplay prompt'''
         try:
             redisplay()
-        except (Exception, NameError) as ex:
+        except NameError:
             pass
             
     def get_prompt(self):
